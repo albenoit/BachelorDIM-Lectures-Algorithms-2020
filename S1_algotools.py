49e347b153cb58a4149945c3ea36ebb6ed725c4e
albentmp <albentmp@iut-acy.local>
2020-09-15 15:49:12 +0200

    max value and index function

--- S1_algotools.py
+++ S1_algotools.py
@@ -16,10 +16,12 @@ et donc on essaye de faire une division par 0 ce qui renvoie une erreur.
 """
 
 def average_above_zero(table:list):
+    """Variables initialization"""
     sum = 0
     n = 0
     tab_size = len(table)
     for x in range(0, tab_size):
+        """Check if our value is positive"""
         if table[x]>0:
             sum += table[x]
             n += 1
@@ -27,3 +29,14 @@ def average_above_zero(table:list):
     return average
 
 print(average_above_zero([1, 2, 3]))
+
+def max_value(tab:list):
+    """Variables initialization"""
+    max_var = max(tab)
+    max_var_index = tab.index(max_var)
+    return max_var, max_var_index
+
+print(max_value([1, 2, 3]))
+    
+            
+        
