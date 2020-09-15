47dc16794b56ff0b3eed68fb68fb2b2b957b09bd
Loic VIARD-CRETAT <viardcrl@iut-acy.local>
2020-09-15 16:18:25 +0200

    Reverse_a_table

--- S1_algotools.py
+++ S1_algotools.py
@@ -8,15 +8,17 @@ Created on Tue Sep 15 14:44:40 2020
 import math;
 
 ##
-# \package mypackage
+# \package math
 # \author viardcrl
 # \details returns the average of an array passed in parameter
 #
 # Question : What happens if Som initialization is forgotten ?
 # UnboundLocalError: local variable 'Som' referenced before assignment
+# La variable sum n'existe pas
 #
 # Question : What can you expect if all the values are below zero ?
 # AttributeError: module 'math' has no attribute 'float'
+# On ne peut pas diviser 0
 #
 def average_above_zero(table:list):
     Som = 0;
@@ -27,22 +29,26 @@ def average_above_zero(table:list):
             Som += i;
             N += 1;
     Moy = math.floor(Som / N);
-    return Moy;
+    return "Average :", Moy;
 
 print(average_above_zero([1,5,3,4,9,5]));
 
 ##
-# \package mypackage
+# \package math
 # \author viardcrl
-# \details returns the average of an array passed in parameter
-#
-# Question : What happens if Som initialization is forgotten ?
-# UnboundLocalError: local variable 'Som' referenced before assignment
-#
-# Question : What can you expect if all the values are below zero ?
-# AttributeError: module 'math' has no attribute 'float'
+# \details returns the index and max value of an array passed in parameter
 #
 def max_value(table:list):
     return "Index :", table.index(max(table)), "Value :", max(table);
+
+print(max_value([1,5,3,4,9,5]));
+
+##
+# \package math
+# \author viardcrl
+# \details returns the index and max value of an array passed in parameter
+#
+def max_value(table:list):
+    return "Reversed Table :", table[::-1];
 
 print(max_value([1,5,3,4,9,5]));
