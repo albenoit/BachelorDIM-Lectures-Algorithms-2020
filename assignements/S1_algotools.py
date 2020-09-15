0752086791f91bf6ec81cf9f4803dcc07de03f27
PLUME <lucas.ru74@gmail.com>
2020-09-15 16:57:21 +0200

    MaxValue_02

--- assignements/S1_algotools.py
+++ assignements/S1_algotools.py
@@ -1,3 +1,5 @@
+list = [12, 14, 16, 17, 18]
+
 def average_above_zero(table:list):
     '''
         This function calculate the average of a list 
@@ -15,25 +17,7 @@ def average_above_zero(table:list):
     Moy = som/n
     return print(float(Moy))
 
-def max_value(table:list):
-    '''
-        This function search the maximum value of a table
-        Parameters:
-            table: List of number
-        Returns:
-            the maximum value of a table
-    '''
-    max_nb=0
-    for i in range(1, len(table)):
-        if table[i] > 0:
-            if table[i] > max_nb:
-                max_nb = table[i] 
-    return print(float(max_nb),int(max_nb))
-
-
-list = [12, 14, 16, 17, 18]
 average_above_zero(list)
-max_value(list)
 
 # What happens if Som initialization is forgotten ?
 
@@ -45,3 +29,25 @@ max_value(list)
 
 #  On a le message d'erreur: ZeroDivisionError: division by zero
 # Cela arrive quand l'on ne rentre pas dans la condition p_list[i] > 0 et donc que n reste nulle 
+
+
+def max_value(table:list):
+    '''
+        This function search the maximum value of a table
+        Parameters:
+            table: List of number
+        Returns:
+            the maximum value of a table
+    '''
+    max_nb=0
+    max_id=0
+    for i in range(1, len(table)):
+        if table[i] > 0 and table[i] > max_nb:
+            max_nb = table[i] 
+            max_id = i
+    return print(float(max_nb),int(max_id))
+
+
+max_value(list)
+
+
