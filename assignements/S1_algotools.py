30f803b80bccb368a5f7d7791b34bf0087f60ae3
Florent GLAUDA <glaudaf@iut-acy.local>
2020-09-15 16:46:41 +0200

    Create function to get average of positive numbers

--- assignements/S1_algotools.py
+++ assignements/S1_algotools.py
@@ -1,9 +1,26 @@
-# -*- coding: utf-8 -*-
-"""
-Éditeur de Spyder
+def average_above_zero(tab):
+    '''
+    Function to add average of positive numbers in array
+    This function add one array of numbers in args
+    Parameters :
+        tab: array of numbers
+    Returns :
+        the average of positive numbers
+    '''
+    som=0 #Sum of positive numbers in array. Init at 0
+    N=0 #Count of positive numbers in array. Init at 0
+    
+    for i in range(len(tab)):
+        if tab[i] > 0: # If number is > 0
+            som = som + tab[i]
+            N = N + 1
+    
+    # Calculate the average
+    average = som / N
 
-Ceci est un script temporaire.
-"""
+    return average # Display in console
 
-Som=0
-N=0
+# Call the function with array in args
+print('Average : ')
+print(average_above_zero([1,10,2,8,-2]))
+
