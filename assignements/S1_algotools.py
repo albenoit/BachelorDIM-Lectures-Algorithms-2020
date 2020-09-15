26fdc25002026b52f0c43b8f0dd6d9c320073449
Valentin Tapie <valentin.tapie@etu.univ-smb.fr>
2020-09-15 16:13:11 +0200

    Average 1

--- assignements/S1_algotools.py
+++ assignements/S1_algotools.py
@@ -6,5 +6,28 @@ Ceci est un script temporaire.
 """
 
 
+"""
+Session 1 Averaging 
+"""
 
-print("yo")
+"""
+Questions
+What happens if Som initialization is forgotten ?
+    if Som already existed before this code, then Sum will already have a value
+    
+What can you expect if all the values are below zero ?
+    N value will be 0 and because we divide a number with N there will be an error 
+
+Translate this algorithm in the python3.x language within script of name:
+"""
+def average_above_zero(Tab):
+    Som = 0
+    N = 0 
+    for i in Tab :
+        if i > 0 :
+            Som = Som + i
+             N += 1
+    Moy = Som/N
+    return Moy
+
+print(average_above_zero([1,2,3]))
