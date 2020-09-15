bdfcf67709d856359d707a788e05794115bb4507
Antoine BOUVARD <bouvaran@iut-acy.local>
2020-09-15 16:18:05 +0200

    Function Reverse Table

--- assignements/S1_algotools.py
+++ assignements/S1_algotools.py
@@ -18,7 +18,7 @@ def average_above_zero(Thetab):
     """@function average_above_zero
     som = Contains the sum of the numbers in Thetab
     n = contains the number of loops
-    return the average of Thetab
+    return = the average of Thetab
     """
     som = 0
     n = 0
@@ -37,7 +37,7 @@ print("Average : " + str(average_above_zero(Thetab)))
 def max_value(Thetab):
     """@function max_value
     save = saves the highest number
-    return the highest number
+    return = the highest number
     """
     save = 0
     
@@ -47,5 +47,39 @@ def max_value(Thetab):
             
     return save
 print("Max : " + str(max_value(Thetab)))
+
+
+#////////////////////////////////////////////
+def reverse_table(Thetab):
+    """@function reverse_table
+    return = reverse Thetab
+    """
+    return Thetab[::-1]
+print("Reverse : " + str(reverse_table(Thetab)))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
