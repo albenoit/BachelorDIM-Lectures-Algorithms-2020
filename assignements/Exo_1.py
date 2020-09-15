76f72e832a5b7e9088511f5d6e08ae05920ac9e8
CAANASUUCRE <alexis.vanhoutte59@gmail.com>
2020-09-15 16:47:03 +0200

    Fonction maximum_value ajouté

--- assignements/Exo_1.py
+++ assignements/Exo_1.py
@@ -22,11 +22,28 @@ def average_above_zero(table:list):
             N += 1
             
     Moy = Som/N
-    print(Moy)
+    return Moy
 
-average_above_zero(Tab)
-
-# 1. Si Som n'est pas défini, Som ne s'initialisera jamais à 0 lors du
-#   du lancement du programme
+print(average_above_zero(Tab))
+# 1. Si Som n'est pas défini, le programme ne fonctionne plus
 # 2. Si toutes les valeus sont en dessous de 0, N restera à 0 et donc une
 #   erreur apparaitra à "Moy = Som/N"
+
+def maximum_value(table:list):
+    '''
+        This function find the highest value in the list
+        Args:
+            table: list of number
+        Returns the maximum value of a list
+    '''
+    maxValue = 0
+    index = 0
+    for i in range (1,len(table)):
+        if(table[i] > maxValue):
+            maxValue = table[i]
+            index = i+1
+    return maxValue,index
+    
+(maxi,index) = maximum_value(Tab)
+print("Valeur Max : {} , index de la valeur max : {}".format(maxi,index))
+
