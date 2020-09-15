4a0b54abed74879ecc37e9d5d56749dfd5e04a44
Lucas Encrenaz <lucas.encrenaz@yahoo.fr>
2020-09-15 16:57:04 +0200

    Table maximum value01

--- assignements/S1_algotools.py
+++ assignements/S1_algotools.py
@@ -17,8 +17,8 @@ def average_above_zero(table:list):
     moy = som/n
     return print(moy)
 
-list = [8, 15, 14, 12, 6, 18, 10, 2, 20]
-average_above_zero(list)
+# list = [8, 15, 14, 12, 6, 18, 10, 2, 20]
+# average_above_zero(list)
 
 # What happens if Som initialization is forgotten ?
 # On a une erreur qui nous dit que Som n'est pas défini NameError: name 'som' is not defined
@@ -27,3 +27,21 @@ average_above_zero(list)
 # On a un message d'erreur qui nous dit qu'on ne peut pas diviser par 0 : ZeroDivisionError: division by zero
 # Et on vérifie ça avec le if (p_list[i] > 0):
 
+def get_maximum_value(table:list):
+    '''
+    This function found the max value of a list of numbers
+    
+    Parameters :
+        list : the list of numbers
+    
+    Return : 
+        the max value of a list
+    '''
+    max=0
+    for i in range(1, len(table)) :
+        if (table[i] > max):
+            max = table[i]
+    return print(max)
+
+list = [8, 15, 14, 12, 6, 18, 10, 2]
+get_maximum_value(list)
