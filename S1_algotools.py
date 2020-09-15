b3443ecc77dde46a5d0fc82127d3138c6ddbaeec
albentmp <albentmp@iut-acy.local>
2020-09-15 16:09:34 +0200

    Add function average_above_zero()

--- S1_algotools.py
+++ S1_algotools.py
@@ -5,10 +5,13 @@
 Ceci est un script temporaire.
 """
 
-Som = 0
+def average_above_zero(table:list):
+    Som = 0
+    for i in table:
+        if(i > 0):
+            Som = Som + i
+    Moy = Som/len(table)
+    return(Moy)
+    
 Tab=(10,21,32,43)
-for i in Tab:
-    if(i > 0):
-        Som = Som + i
-Moy = Som/len(Tab)
-print(Moy)
+print(average_above_zero(Tab))
