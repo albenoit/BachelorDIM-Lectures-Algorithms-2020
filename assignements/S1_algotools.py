5bebe917fa8fc4764e277bfc27c5b1c0e604811b
Guillian VIBERT-VALLET <vibertvg@iut-acy.local>
2020-09-15 16:18:39 +0200

    Création de la moyenne

--- assignements/S1_algotools.py
+++ assignements/S1_algotools.py
@@ -5,3 +5,12 @@ Created on Tue Sep 15 15:28:10 2020
 @author: vibertvg
 """
 
+som=0
+N=0
+Tab=[12,18,6,20,14,5,6,3,18,10]
+for i in Tab:
+    if i > 0:
+        som = som + i
+        N = N + 1
+Moy = som/N
+print(Moy)
