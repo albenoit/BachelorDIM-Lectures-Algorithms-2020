0708443a9e3ad87399c4c54ccefe40b844acbc94
Baptiste POLLET <polletb@iut-acy.local>
2020-09-15 15:28:35 +0200

    Algo_2

--- S1_algotoolspy.py
+++ S1_algotoolspy.py
@@ -5,8 +5,13 @@ Created on Tue Sep 15 14:45:46 2020
 @author: polletb
 """
 import math
-
-def algorithm_1(marks):
+##
+#   \package algo_1
+#   \autor polletb
+#   \brief get average from array
+#   \params @marks = array
+#
+def average_above_zero(marks:list):
     average = 0    
     
     for i in marks:
@@ -16,4 +21,10 @@ def algorithm_1(marks):
     return average
 
 
-print(algorithm_1([10, 20]))
+#print(average_above_zero([10.0, 20.0]))
+
+def max_value(marks:list):
+    return max(marks)
+
+print(max_value([10.0, 20.0, 30.0]))
+    
