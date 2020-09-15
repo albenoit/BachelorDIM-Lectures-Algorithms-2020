352380bf79b8b1033ee5de6520309299e450e55e
Loic VIARD-CRETAT <viardcrl@iut-acy.local>
2020-09-15 17:29:24 +0200

    Roi_bbox

--- S1_algotools.py
+++ S1_algotools.py
@@ -6,21 +6,29 @@ Created on Tue Sep 15 14:44:40 2020
 """
 
 import math;
+import numpy as np
 
-##
-# \package math
-# \author viardcrl
-# \details returns the average of an array passed in parameter
-#
-# Question : What happens if Som initialization is forgotten ?
-# UnboundLocalError: local variable 'Som' referenced before assignment
-# La variable sum n'existe pas
-#
-# Question : What can you expect if all the values are below zero ?
-# AttributeError: module 'math' has no attribute 'float'
-# On ne peut pas diviser 0
-#
+'''
+    Question : What happens if Som initialization is forgotten ?
+        UnboundLocalError: local variable 'Som' referenced before assignment
+        La variable sum n'existe pas
+    
+    Question : What can you expect if all the values are below zero ?
+        AttributeError: module 'math' has no attribute 'float'
+        On ne peut pas diviser 0
+'''
 def average_above_zero(table:list):
+    '''
+    Calculate the sum of a list of integer
+        package:
+            math
+        author:
+            viardcrl
+        Parameters:
+            table: list of integer
+        Returns:
+            returns the average of an array passed in parameter
+    '''
     Som = 0;
     N = 0;
     
@@ -33,22 +41,52 @@ def average_above_zero(table:list):
 
 print(average_above_zero([1,5,3,4,9,5]));
 
-##
-# \package math
-# \author viardcrl
-# \details returns the index and max value of an array passed in parameter
-#
+
 def max_value(table:list):
+    '''
+        package:
+            math
+        author:
+            viardcrl
+        Parameters:
+            table: list of integer
+        Returns:
+            returns the index and max value of an array passed in parameter
+    '''
     return "Index :", table.index(max(table)), "Value :", max(table);
 
 print(max_value([1,5,3,4,9,5]));
 
-##
-# \package math
-# \author viardcrl
-# \details returns the index and max value of an array passed in parameter
-#
-def max_value(table:list):
+
+def reverse_table(table:list):
+    '''
+        package:
+            math
+        author:
+            viardcrl
+        Parameters:
+            table: list of integer
+        Returns:
+            returns an inversed array
+    '''
     return "Reversed Table :", table[::-1];
 
-print(max_value([1,5,3,4,9,5]));
+print(reverse_table([1,5,3,4,9,5]));
+
+
+def roi_bbox(input_image:np):
+    '''
+        package:
+            math, numpy
+        author:
+            viardcrl
+        Parameters:
+            input_image: 
+        Returns:
+            returns 
+    '''
+    input_image[2:5,2:5] = np.ones((3,3),dtype=float);
+    return input_image;
+
+print(roi_bbox(np.zeros((10,10), dtype=float)));
+
