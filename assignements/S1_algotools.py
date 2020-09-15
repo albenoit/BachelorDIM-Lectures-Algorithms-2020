1303357c817e142cef67ea0e80a519c6ddf6c45e
Guillian VIBERT-VALLET <vibertvg@iut-acy.local>
2020-09-15 17:34:57 +0200

    Ajout de la fonction max value

--- assignements/S1_algotools.py
+++ assignements/S1_algotools.py
@@ -5,6 +5,8 @@ Created on Tue Sep 15 15:28:10 2020
 @author: vibertvg
 """
 
+my_array=[6,18]
+
 def average_above_zero(array_of_data):
     """
         Function who work with an array of numbers greater than 0
@@ -27,7 +29,6 @@ def average_above_zero(array_of_data):
         raise ValueError('problem, a number is equivalent or under 0')
     return float(Moy)
 
-my_array=[0,6,18]
 moy=average_above_zero(my_array)
 print('my average',moy)
 
@@ -39,3 +40,30 @@ print('my average',moy)
     What can you expect if all the values are below zero ? 
         We cant divide by zero. It will make an error
 """
+
+def max_value(array_of_data):
+    """
+        Function who work with an array of numbers greater than 0
+        Parameters:
+            som: the addition of all number average
+            N: the number of iterations (équivalant to the size of the array)
+            Moy: the sum of all numbers divided by the number of itérations
+        Returns:
+            The average calculated with the variable Moy
+    """
+    N=0
+    highest_number=0
+    for i in array_of_data:
+        if i > 0:
+            N += 1
+            if i > highest_number:
+                highest_number = i
+    if N < 0:
+        raise ValueError('problem, a number is equivalent or under 0')
+    return float(highest_number)
+
+
+max_val=max_value(my_array)
+print('the max value is',max_val)
+
+
