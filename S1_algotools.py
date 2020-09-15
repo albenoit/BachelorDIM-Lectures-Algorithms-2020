ab9344f3a699bbcc1540a0369c49af2c21dd1537
albentmp <albentmp@iut-acy.local>
2020-09-15 15:38:04 +0200

    function average_above_zero update

--- S1_algotools.py
+++ S1_algotools.py
@@ -5,15 +5,25 @@
 Ceci est un script temporaire.
 """
 
-def average(tab):
+"""
+Q1 / Si on oublie l'initialisation de la variable on ne pourra pas l'utiliser 
+dans le for ensuite.
+"""
+
+"""
+Q2 / Si toutes les variables sont en dessous de 0, on ne passe pas dans le if, 
+et donc on essaye de faire une division par 0 ce qui renvoie une erreur.
+"""
+
+def average_above_zero(table:list):
     sum = 0
     n = 0
-    tab_size = len(tab)
+    tab_size = len(table)
     for x in range(0, tab_size):
-        if tab[x]>0:
-            sum += tab[x]
+        if table[x]>0:
+            sum += table[x]
             n += 1
     average = sum/n
     return average
 
-print(average([1, 2, 3]))
+print(average_above_zero([1, 2, 3]))
