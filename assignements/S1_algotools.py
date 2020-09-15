57d22cb5b9b5f4f77fbab3fd5634158634f37fb3
Yoan <yoan-rouleau@live.fr>
2020-09-15 16:39:29 +0200

    getAverage() function ready + 2 firsts question answers

--- assignements/S1_algotools.py
+++ assignements/S1_algotools.py
@@ -3,16 +3,32 @@ Created by Yoan ROULEAU
 @author: myself
 """
 
-Som=0
-N=0
-Tab = [1, 2]
-for i in Tab:
-    if i > 0:
-        Som = Som + i
-        N = N + 1
-Moy = Som/N
-print(Moy)
+Tab = [1, 2, 3]
+def getAverage(array):
+    '''
+    Receive an array as a parameter and calculates its average.
+    :arg
+        array: an array
+    :returns
+        Its average
+    '''
+    som = 0
+    n = 0
+    for i in array:
+        if i > 0:
+            som = som + i
+            n = n + 1
+    moy = som/n
+    return moy
 
-#WHAT HAPPENS IF "SOM" INITIALIZATION IS FORGOTTEN ?
-#-> You get an error saying that Som isn't defined.
+print(getAverage(Tab))
+
+"""
+WHAT HAPPENS IF "SOM" INITIALIZATION IS FORGOTTEN ?
+-> You get an error saying that Som isn't defined.
+
+WHAT CAN YOU EXPECT IF ALL THE VALUES ARE BELLOW ZERO ?
+-> If your values are bellow zero, you wont be able to access the average calculation since you're testing each 
+values in the array are bellow zero.
+"""
 
