16e1fd817483a8f05ea94523ba486d4f32e050e9
albentmp <albentmp@iut-acy.local>
2020-09-15 15:53:43 +0200

    Translate subject algo

--- S1_algotools.py
+++ S1_algotools.py
@@ -5,4 +5,12 @@
 Ceci est un script temporaire.
 """
 
-print("Bonjour")
+Som = 0
+N = 0
+Tab=(10,21,32,43)
+for i in Tab:
+    if(i > 0):
+        Som = Som + i
+        N = N+1
+Moy = Som/N
+print(Moy)
