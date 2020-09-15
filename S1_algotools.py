da442d82aacd6ec7baef4bb28664dab2940d6e40
Loic VIARD-CRETAT <viardcrl@iut-acy.local>
2020-09-15 15:53:11 +0200

    Table_maximum_value

--- S1_algotools.py
+++ S1_algotools.py
@@ -16,7 +16,7 @@ import math;
 # UnboundLocalError: local variable 'Som' referenced before assignment
 #
 # Question : What can you expect if all the values are below zero ?
-#
+# AttributeError: module 'math' has no attribute 'float'
 #
 def average_above_zero(table:list):
     Som = 0;
@@ -26,7 +26,23 @@ def average_above_zero(table:list):
         if i > 0:
             Som += i;
             N += 1;
-    Moy = math.float(Som / N);
+    Moy = math.floor(Som / N);
     return Moy;
 
-print(average_above_zero([-1,-5,-3,-4,-9,-5]));
+print(average_above_zero([1,5,3,4,9,5]));
+
+##
+# \package mypackage
+# \author viardcrl
+# \details returns the average of an array passed in parameter
+#
+# Question : What happens if Som initialization is forgotten ?
+# UnboundLocalError: local variable 'Som' referenced before assignment
+#
+# Question : What can you expect if all the values are below zero ?
+# AttributeError: module 'math' has no attribute 'float'
+#
+def max_value(table:list):
+    return "Index :", table.index(max(table)), "Value :", max(table);
+
+print(max_value([1,5,3,4,9,5]));
