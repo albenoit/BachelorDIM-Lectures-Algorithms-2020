8a00c56e4bdab552a829d21220ac80ae7b5a9039
Just1bridou <45208166+Just1bridou@users.noreply.github.com>
2020-09-15 15:45:52 +0200

    feat: get maximum value of list

--- assignements/S1_algotools.py
+++ assignements/S1_algotools.py
@@ -1,29 +1,16 @@
 tab = [1,2,3,4,5,6,7,8,9]
 
-def average_above_zero(tab):
-    """function average_above_zero(tab)
+def max_value(tab):
+    """function max_value(tab)
     
-    return average of list 'tab'
+    return maximum value of list 'tab'
     """
-    som = 0
-    n = 0
+    max_val = 0
 
     for i in tab:
-        if i > 0:
-            som += i
-            n += 1
+        if i > max_val:
+            max_val = i
 
-    return som / n
+    return max_val
 
-print(average_above_zero(tab))
-
-"""
-1- If Som isn't initalized, program throw error (NameError) because 
-   we're using it in loop.
-
-2- Throw error because n will be equal to 0 and we're splitting som by n (x/0)
-
-3- /
-
-4- /
-"""
+print(max_value(tab))
