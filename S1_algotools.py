6e9fdfa3311c9167a2d4cba8d9fe702d55112883
albentmp <albentmp@iut-acy.local>
2020-09-15 17:30:10 +0200

    bounding box in progress

--- S1_algotools.py
+++ S1_algotools.py
@@ -15,6 +15,8 @@ Q2 / Si toutes les variables sont en dessous de 0, on ne passe pas dans le if,
 et donc on essaye de faire une division par 0 ce qui renvoie une erreur.
 """
 
+import numpy as np
+
 def average_above_zero(table:list):
     """Variables initialization"""
     sum = 0
@@ -41,7 +43,43 @@ def reverse_table(tab:list):
     tab=tab[::-1]
     return tab;
 
-print(reverse_table([1, 2, 3]))
+#print(reverse_table([1, 2, 3]))
+
     
-            
+def roi_bbox(input_image): 
+    
+    #Variables
+    rows = input_image.shape[0]
+    cols = input_image.shape[1]
+    
+    #Each coordinates
+    left_up = [0,0]
+    left_down = [(rows-1),0]
+    right_up = [0, (cols-1)]
+    right_down = [(rows-1), (cols-1)]
+    
+    #Return
+    z = ([left_up, right_up], [right_down, left_down])
+    return z
+    
+lines = 5
+cols = 5
+input_image = np.zeros((lines, cols))   
+#print(roi_bbox(input_image))
+test = input_image[1:3,2:4] = np.ones((2, 3))
+print(test)
+'''     
+for a in range(2,5):
+    for b in range(1, 3):
+        input_image[b, a] = 1
+print(input_image)
+'''
+
+'''
+input_image[1:3,2:4] = np.ones((2, 3))
+'''
+
+
+
+  
         
