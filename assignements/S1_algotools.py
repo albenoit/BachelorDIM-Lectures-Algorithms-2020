c7bb63dff63a6ac7f022be7220bc801460879010
Antoine BOUVARD <bouvaran@iut-acy.local>
2020-09-15 16:09:04 +0200

    Function Max Value

--- assignements/S1_algotools.py
+++ assignements/S1_algotools.py
@@ -1,29 +1,51 @@
 # -*- coding: utf-8 -*-
 """
 Created on Tue Sep 15 14:14:06 2020
-
 @author: bouvaran
 """
 
+Thetab = [1,2,3,4,5,6,7,8,9]
 
+#////////////////////////////////////////////
 """
-Q1 : If "som" isn't initialized, the results adds up
-Q2 : I have an error, I can't split by 0 
-     "ZeroDivisionError: division by zero"
+Q1 :If "som" isn't initialized, I have an error because the 
+    variable have to be referenced before assignement
+Q2 :I have an error, I can't split by 0 
+    "ZeroDivisionError: division by zero"
 """
 
-
-tabMax = [1,2,3,4,5,6,7,8,9]
-def average_above_zero(tabMax):
+def average_above_zero(Thetab):
+    """@function average_above_zero
+    som = Contains the sum of the numbers in Thetab
+    n = contains the number of loops
+    return the average of Thetab
+    """
     som = 0
     n = 0
     
-    for i in tabMax :
+    for i in Thetab :
         if i > 0 :
             som += i
             n += 1
             
     Moy = som / n
     return float(Moy)
+print("Average : " + str(average_above_zero(Thetab)))
 
-print(average_above_zero(tabMax))
+
+#////////////////////////////////////////////
+def max_value(Thetab):
+    """@function max_value
+    save = saves the highest number
+    return the highest number
+    """
+    save = 0
+    
+    for i in Thetab :
+        if i > save :
+            save = i
+            
+    return save
+print("Max : " + str(max_value(Thetab)))
+
+
