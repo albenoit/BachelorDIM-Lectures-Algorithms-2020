f2dba1e248dd422cae5aee07326a21aa8d521201
GF Création Web <skeul73@live.fr>
2020-09-16 11:47:49 +0200

    Add bounding box function

--- assignements/S1_algotools.py
+++ assignements/S1_algotools.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 def average_above_zero(tab :list):
     '''
     Function to add average of positive numbers in array
@@ -43,10 +45,10 @@ def max_value(tab :list):
     return maxNumberIndex, maxNumber # Return index of max number & the max number
 
 # Call the function with array in args and display the index of max value
-print('# Max value index function #')
-print('T1 = ', max_value([1,10,2,8,11])) #Only positive numbers
-print('T2 = ', max_value([1,10,2,8,-11])) #With positive and negative numbers
-print('T3 = ', max_value([-1,-10,-2,-8,-11])) #Only negative numbers
+# print('# Max value index function #')
+# print('T1 = ', max_value([1,10,2,8,11])) #Only positive numbers
+# print('T2 = ', max_value([1,10,2,8,-11])) #With positive and negative numbers
+# print('T3 = ', max_value([-1,-10,-2,-8,-11])) #Only negative numbers
 
 # Reverse table V1
 def reverse_table(tab :list):
@@ -62,8 +64,8 @@ def reverse_table(tab :list):
     return tab
 
 # Call the function with array in args and display reversed array
-print('# Reverse table function #')
-print(reverse_table([1,10,2,8,11]))
+# print('# Reverse table function #')
+# print(reverse_table([1,10,2,8,11]))
 
 # Reverse table V2
 def reverse_table_V2(tab : list):
@@ -81,8 +83,8 @@ def reverse_table_V2(tab : list):
     return tab
 
 # Call the function with array in args and display reversed array 
-print('# Reverse table function V2 #')
-print(reverse_table_V2([1,10,2,8,11]))
+# print('# Reverse table function V2 #')
+# print(reverse_table_V2([1,10,2,8,11]))
 
 # Reverse table V3
 def reverse_table_V3(tab : list):
@@ -101,6 +103,45 @@ def reverse_table_V3(tab : list):
     return tab
 
 # Call the function with array in args and display reversed array 
-print('# Reverse table function V3 #')
-print(reverse_table_V3([1,10,2,8,11]))
+# print('# Reverse table function V3 #')
+# print(reverse_table_V3([1,10,2,8,11]))
+
+
+
+# Bounding box
+H=12
+W=10
+matrix=np.zeros((H,W))
+# matrix[2:3, 3:5]= np.ones((2,2)) * 2
+for c in range(1,3):
+    for l in range(3,5):
+        matrix[l,c] = 1
+for c in range(5,8):
+    for l in range(6,9):
+        matrix[l,c] = 1
+print(matrix)
+
+def roi_bbox(img):
+    '''
+    Function to get coordinates of img in matrix
+    Parameters :
+        img: matrix of img
+    Returns :
+        the coordinates of img
+    '''
+    x1 = 0
+    y1 = 0
+    x2 = 0
+    y2 = 0
+
+    targets = np.argwhere(img > 0)
+    # print(targets)
+
+    x1 = targets[0][1]
+    y1 = targets[0][0]
+    x2 = targets[len(targets) - 1][1]
+    y2 = targets[len(targets) - 1][0]
+    print(np.array([x1, y1, x2, y2]))
+
+roi_bbox(matrix)
 
