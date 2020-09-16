dc447cfc56b01edba4031741aaa85e39b52300a6
Baptiste POLLET <polletb@iut-acy.local>
2020-09-16 09:03:51 +0200

    Algo_4

--- S1_algotoolspy.py
+++ S1_algotoolspy.py
@@ -16,18 +16,21 @@ def average_above_zero(marks:list):
     Returns :
         average of list
     '''
-    average = 0    
+    average = 0  
+    try :
+        for i in marks:
+            average += i
+        
+        average = math.floor(average / len(marks))
+    except:
+        raise Exception("Sorry average is null or equal 0") 
     
-    for i in marks:
-        average += i
-    
-    average = math.floor(average / len(marks))
     return average
 
 #Question 1 : la variable sum n'existe pas donc erreur
 #Question 2 : erreur pas divisible par 0
 
-#print(average_above_zero([10.0, 20.0]))
+#print(average_above_zero([10.0,20.0]))
 
 
 def max_value(marks:list):
@@ -38,7 +41,13 @@ def max_value(marks:list):
     Returns :
         max of list
     '''
-    return max(marks), marks.index(max(marks))
+    try:
+       max_value = max(marks) 
+       max_value_index = marks.index(max(marks))
+    except:
+        raise Exception("Sorry marks is null, empty")
+    
+    return max_value, max_value_index
 
 #print(max_value([10.0, 20.0, 30.0]))
     
@@ -51,7 +60,12 @@ def reverse_table(table:list):
     Returns :
         list reverse
     '''
-    return table[::-1]
+    try:
+        reverse_table = table[::-1]
+    except:
+        raise Exception("Sorry table is null or empty")
+        
+    return reverse_table
 
 #print(reverse_table([10,20]))
  
@@ -62,10 +76,26 @@ def reverse_table(table:list):
 #   \params @table = array
 #
 def roi_bbox(input_image:np):
-    input_image[2:5, 2:5] = np.ones((3,3), dtype=float)
-    
+    '''
+    Get bouding box of numpy array with value equal 1
+    Parameters:
+            input_image: numpy
+    Returns :
+        input_image as numpy
+        tuples with coords of min and max bounding box
+    '''
+    try:
+        input_image[2:5, 2:5] = np.ones((3,3), dtype=float)
         
-    return input_image
+        row = np.any(input_image, axis=1)
+        column = np.any(input_image, axis=0)
+    
+        rmin, rmax = np.where(row)[0][[0, -1]]
+        cmin, cmax = np.where(column)[0][[0, -1]]
+    except:
+        raise Exception("Sorry numpy is null or empty")
+        
+    return  input_image, ([rmin, cmin], [rmax, cmax])
 
 print(roi_bbox(np.zeros((10,10), dtype=float)))
 
