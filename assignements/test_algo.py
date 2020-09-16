4e1776003dae3b973cf86c7bdd17a671f7399cc2
Just1bridou <45208166+Just1bridou@users.noreply.github.com>
2020-09-16 15:16:51 +0200

    update pytest

--- assignements/test_algo.py
+++ assignements/test_algo.py
@@ -1,9 +1,13 @@
 import S1_algotools as algo
+import pytest
 
 tab = [1,9,3,4,5,6,7,8,2]
+tab_under_zero = [0,-1,-3,-2]
 
 def test_average():
     assert algo.average_above_zero(tab) == 5
+    with pytest.raises(ValueError):
+        assert algo.average_above_zero(tab_under_zero)
 
 def test_max_value():
     assert algo.max_value(tab) == 9
