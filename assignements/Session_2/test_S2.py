e79a187c6db6faac2070377ff932acaded11faeb
CAANASUUCRE <alexis.vanhoutte59@gmail.com>
2020-09-16 15:47:10 +0200

    Test unitaire

--- assignements/Session_2/test_S2.py
+++ assignements/Session_2/test_S2.py
@@ -5,11 +5,6 @@ Created on Wed Sep 16 14:37:36 2020
 @author: vanhouta
 """
 import function as functest
-#import sys
-#from Session_1 import Exo_1 as funcS1
-#sys.path.insert(1, '../Session_1')
-
-#import S1_algotools as funcS1
 
 
 def test_answer():
@@ -18,6 +13,4 @@ def test_answer():
 def test_answer2():
     assert functest.func(14) == 15
     
-'''def test_average_above_zero():
-    Tab = [1,5,9,8,7,5,9,6,7,2]
-    assert funcS1.average_above_zero(Tab) == 6.444444444444445'''
+print(functest.func(4))
