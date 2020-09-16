82f0e0ec9c5c22cd202bd127b9167402a89c214d
CAANASUUCRE <alexis.vanhoutte59@gmail.com>
2020-09-16 15:25:43 +0200

    Test unitaire average_above_zero

--- assignements/Session_2/test_S2.py
+++ assignements/Session_2/test_S2.py
@@ -5,9 +5,14 @@ Created on Wed Sep 16 14:37:36 2020
 @author: vanhouta
 """
 import function as functest
+import Session_1.Exo_1 as funcS1
 
 def test_answer():
     assert functest.func(3) == 4
     
 def test_answer2():
     assert functest.func(14) == 15
+    
+def test_average_above_zero():
+    Tab = [1,5,9,8,7,5,9,6,7,2]
+    assert funcS1.average_above_zero(Tab) == 6.444444444444445
