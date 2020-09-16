17f2f615162f27eff23e33765e9d25632de2bb74
PLUME <lucas.ru74@gmail.com>
2020-09-16 16:48:36 +0200

    Test_02

--- assignements/Session2/test_S2.py
+++ assignements/Session2/test_S2.py
@@ -9,6 +9,7 @@ for c in range(12,14):
     for l in range(13,15):
         Xin[l,c]=1
 testTable=[1,3,5,7,9,2,4,6,8]
+testTable2=[-13,-57,92,-46,8]
 
 def test_answer():
     assert algo.onemore(3) == 4
@@ -23,14 +24,18 @@ def test_myaddition_wrong_input():
     with pytest.raises(TypeError):
         algo.my_addition('a',2)
 
-def test_average_above_zero():
-    assert algo.average_above_zero(testTable) == 5.625
+# def test_average_above_zero():
+#     assert algo.average_above_zero(testTable) == 5.625
 
-def test_max_value():
-    assert algo.max_value(testTable) == 9
+# def test_average_above_zero2():
+#     with pytest.raises(TypeError):
+#         algo.average_above_zero(testTable2)
 
-def test_reverse_table():
-    assert algo.reverse_table(testTable) == [8, 6, 4, 2, 9, 7, 5, 3, 1]
+# def test_max_value():
+#     assert algo.max_value(testTable) == 9
+
+# def test_reverse_table():
+#     assert algo.reverse_table(testTable) == [8, 6, 4, 2, 9, 7, 5, 3, 1]
 
 # def roi_bbox():
 #     assert algo.roi_bbox(Xin) == [2 4 13 14]
