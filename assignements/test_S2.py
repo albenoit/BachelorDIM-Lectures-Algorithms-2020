fdec559c19a1d67aa1472cf380a04dd2e7566fe3
Yoan <yoan-rouleau@live.fr>
2020-09-16 15:27:14 +0200

    Other extra tests

--- assignements/test_S2.py
+++ assignements/test_S2.py
@@ -1,9 +1,14 @@
 import S1_algotools as tobetested
+import pytest
 
 def test_myaddition_integers():
     assert tobetested.my_addition(10, 2) == 12
 
 def test_myaddition_intergers_2():
     assert tobetested.my_addition(-1, 2) == 1
+
+def test_myaddition_wrong_inpute():
+    with pytest.raises(TypeError):
+        tobetested.my_addition('a', 4)
 
 
