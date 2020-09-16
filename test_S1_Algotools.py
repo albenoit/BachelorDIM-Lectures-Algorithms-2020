5507cb191d37b4a7896e06d8d392d32e12f4245c
Carl FUCHS <fuchsca@iut-acy.local>
2020-09-16 15:17:00 +0200

    Creation of the file

---
+++ test_S1_Algotools.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Sep 16 15:11:11 2020
+
+@author: fuchsca
+"""
+
+import S1_algotools as S1
+
+def test_myaddition_integers():
+    assert S1.alea(100)
+    
