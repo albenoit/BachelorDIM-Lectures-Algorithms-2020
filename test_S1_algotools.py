e964e8d91fa5c8f67419c38697925281b8fd049d
Nathan CHARVIN <charvinn@iut-acy.local>
2020-09-16 15:24:04 +0200

    UnitTest - test_max_value_exception()

--- test_S1_algotools.py
+++ test_S1_algotools.py
@@ -1,4 +1,10 @@
 import S1_algotools as S1
+import pytest
 
-def test_max_value_1():
+def test_max_value_OK():
     assert S1.max_value([0, 10, 20]) == 20
+
+def test_max_value_exception():
+    with pytest.raises(ValueError):
+        S1.max_value([])
+
