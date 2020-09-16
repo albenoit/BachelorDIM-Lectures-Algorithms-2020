f116a602e5b9994d02d83f8792ee2e7ed3773a0b
Valentin Tapie <valentin.tapie@etu.univ-smb.fr>
2020-09-16 15:13:44 +0200

    test envoi valid

--- assignements/test_S2.py
+++ assignements/test_S2.py
@@ -4,12 +4,7 @@ Created on Wed Sep 16 14:21:21 2020
 
 @author: tapiev
 """
-import imp
-
-foo = imp.load_source('S1_algotools', 'S1_algotools.py')
-
-
+from S1_algotools import *
 
 def test_answer():
-    assert foo.average_above_zero([1,2]) == 1.5
-    
+    assert average_above_zero([1,2]) == 1.5
