baab3dc16d44ec3b14d7de3a40f4eef28b2a922e
Enzo LENTZY <lentzye@iut-acy.local>
2020-09-16 15:17:13 +0200

    test unitaire v3

--- test_algo.py
+++ test_algo.py
@@ -2,8 +2,12 @@ import S1_algotools as algo
 
 tab=[1,2,3,4,5]
 
+def test_average():
+    assert algo.average(tab)==3
+
 def test_max_value():
     assert algo.max_value(tab)==5
 
-def test_average():
-    assert algo.average(tab)==3
+def test_reverse_table():
+    assert algo.reverse_table(tab) == [5,4,3,2,1]
+
