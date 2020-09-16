7ff9d34c4fafd557dbdc74d4af0f47b71904751b
Enzo LENTZY <lentzye@iut-acy.local>
2020-09-16 16:46:49 +0200

    test unitaire v8

--- test_algo.py
+++ test_algo.py
@@ -10,7 +10,8 @@ boolean_matrix=np.zeros((12,10),dtype=bool)
 for c in range (7,9):
     for l in range (4,9):
         boolean_matrix[l,c]=1
-
+null_matrix=np.zeros((10,10),dtype= bool)
+char_matrix_empty=np.empty([10,10],dtype=str)
 
 def test_average_1():
     assert algo.average(tab_positive)==3
@@ -18,7 +19,7 @@ def test_average_1():
 def test_average_2():
     with pytest.raises(ValueError):
         assert algo.average(tab_negative)
-        
+
 def test_max_value():
     assert algo.max_value(tab_positive)==5
 
@@ -30,3 +31,11 @@ def test_roi_bbox():
     assert algo.roi_bbox(boolean_matrix)[0][1]==7
     assert algo.roi_bbox(boolean_matrix)[1][0]==8
     assert algo.roi_bbox(boolean_matrix)[1][1]==8
+
+def test_roi_bbox_ValueError():
+    with pytest.raises(ValueError):
+        assert algo.roi_bbox(null_matrix)
+
+def test_random_fill_sparse():
+    a=algo.random_fill_sparse(char_matrix_empty,4)
+    assert len(np.where(a=='X')[0])==4
