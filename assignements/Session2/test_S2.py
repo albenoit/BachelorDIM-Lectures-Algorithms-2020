e91f51295b117a8032cc3cd289c8d6733ad97b30
PLUME <lucas.ru74@gmail.com>
2020-09-16 15:55:15 +0200

    Session folder

--- assignements/Session2/test_S2.py
+++ assignements/Session2/test_S2.py
@@ -1,4 +1,8 @@
+import sys
+sys.path.insert(1,'../Session1')
+
+import S1_algotools as algo
 testTable=[1,3,5,7,9,2,4,6,8]
 
 def test_average_above_zero():
-    assert algo.average_above_zero(testTable) 
+    assert algo.average_above_zero(testTable) == 5.625
