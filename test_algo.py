36709b796ecb29ebceef1861e50c9accdaaac57c
Enzo LENTZY <lentzye@iut-acy.local>
2020-09-16 15:41:18 +0200

    test unitaire v6

--- test_algo.py
+++ test_algo.py
@@ -4,6 +4,8 @@ import numpy as np
 
 tab_positive=[1,2,3,4,5]
 tab_negative=[-1,-2,-3,-4,-5]
+
+
 boolean_matrix=np.zeros((12,10),dtype=bool)
 for c in range (7,9):
     for l in range (4,9):
@@ -22,4 +24,8 @@ def test_max_value():
 def test_reverse_table():
     assert algo.reverse_table(tab_positive) == [5,4,3,2,1]
 
-
+def test_roi_bbox():
+    assert algo.roi_bbox(boolean_matrix)[0][0]==4
+    assert algo.roi_bbox(boolean_matrix)[0][1]==7
+    assert algo.roi_bbox(boolean_matrix)[1][0]==8
+    assert algo.roi_bbox(boolean_matrix)[1][1]==8
