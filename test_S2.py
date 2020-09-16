79313a4c50b9a8f95e2750b5ee3d7e05e2c215c4
Loic VIARD-CRETAT <viardcrl@iut-acy.local>
2020-09-16 17:32:01 +0200

    UnitTest [In progress]

--- test_S2.py
+++ test_S2.py
@@ -7,6 +7,7 @@ Created on Wed Sep 16 14:02:38 2020
 
 import pytest as pt
 import S1_algotools as s1
+import numpy as np
 
 #------------------------------------------------------------------------#
 #------------------------------ EXERCISE 1 ------------------------------#
@@ -72,5 +73,20 @@ def test_reverse_table_3():
         
 def test_reverse_table_4():
     assert s1.reverse_table([1.1,2,9,4.2]) == [4.2,9,2,1.1]
+    
+#------------------------------------------------------------------------#
+#------------------------------ EXERCISE 4 ------------------------------#
+#------------------------------------------------------------------------#
+    
+@pt.fixture
+def zero_numpy():
+    zero_numpy = np.zeros((10,10), dtype=float)
+    return zero_numpy
 
+@pt.fixture
+def zero_numpy_with_one(zero_numpy):
+    zero_numpy[2:5, 2:5] = np.ones((3,3), dtype=float)
+    return zero_numpy
 
+def test_roi_bbox(zero_numpy_with_one, zero_numpy):
+    assert s1.roi_bbox(zero_numpy) == ([2,2],[4,4])
