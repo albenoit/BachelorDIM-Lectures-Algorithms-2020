98470c01bd11ec98b262fe8b53dbbbc63c9600cd
Antoine BOUVARD <bouvaran@iut-acy.local>
2020-09-16 10:12:56 +0200

    Function Random_fill_sparse with some amelioration in other function

--- assignements/S1_algotools.py
+++ assignements/S1_algotools.py
@@ -3,8 +3,8 @@
 Created on Tue Sep 15 14:14:06 2020
 @author: bouvaran
 """
-
-theTab = [1,2,3,4,5,6,7,8,9]
+import numpy as np
+import random
 
 #////////////////////////////////////////////
 """
@@ -24,28 +24,18 @@ def average_above_zero(theTab):
         the average of theTab
     """
     som = 0
-    n = 0
+    n   = 0
     for i in theTab :
         if i > 0 :
             som += i
-            n += 1  
+            n   += 1  
             
     if  n > 0  :  
         Moy = som / n
     else:
         raise ValueError("No positive value found")
-
     return Moy
-print("Average : " + str(average_above_zero(theTab)))
-'''
-my_list=[0,2,-3]
-average = average_above_zero(my_list)
-print("T1 Average : " + str(average))
 
-my_list=9
-average = average_above_zero(my_list)
-print("T2 Average : " + str(average))
-'''
 #////////////////////////////////////////////
 def max_value(theTab):
     """
@@ -61,10 +51,7 @@ def max_value(theTab):
     for i in theTab :
         if i > save :
             save = i
-            
     return save
-print("Max : " + str(max_value(theTab)))
-
 
 #////////////////////////////////////////////
 def reverse_table(theTab):
@@ -74,30 +61,32 @@ def reverse_table(theTab):
     Returns :
         the reverse of theTab
     """
-    return theTab[::-1]
-print("Reverse : " + str(reverse_table(theTab)))
-
+    for i in range(len(theTab)//2):
+        tmp            = theTab[i]
+        endVal         = len(theTab)-i-1
+        theTab[i]      = theTab[endVal]
+        theTab[endVal] = tmp
+    
+    #return theTab[::-1]
+    return theTab
 
 #////////////////////////////////////////////
-import numpy as np
-
-def roi_bbox():
+def roi_bbox(npTab:np.array):
     """
     This function return a Bounding box
     
     Parameters :
         npTab: tab of a numpy array
-        answer: all the coordonates of "1" in npTab
+        matrice: all the coordonates of "1" in npTab
+        x,y: the max and min coordonates
     Returns : 
-        the coordinates of "1" in npTab
+        the coordinates max and min in x and y for 
+        create a bounding box
     """
-    h = 100
-    w = 100
-    x1=None
-    y1=None
-    x2=None
-    y2=None
-    npTab = np.zeros((h,w),dtype = float)
+    x1 = None
+    y1 = None
+    x2 = None
+    y2 = None
     
     #for w in range(45,55):
     #   for h in range(60,85):
@@ -106,7 +95,7 @@ def roi_bbox():
     npTab[60:85,45:55] = np.ones((25,10),dtype = float)   
     
     """
-    matrice = np.argwhere(npTab == 1)
+    matrice    = np.argwhere(npTab == 1)
     for i in matrice:
         if x1 is None or i[0] < x1:
             x1 = i[0]
@@ -118,32 +107,59 @@ def roi_bbox():
             y2 = i[1] 
     """
     matrice = np.where(npTab == 1)
-    x1 = np.min(matrice[1])
-    y1 = np.min(matrice[0])
-    x2 = np.max(matrice[1])
-    y2 = np.max(matrice[0])
-
+    x1      = np.min(matrice[1])
+    y1      = np.min(matrice[0])
+    x2      = np.max(matrice[1])
+    y2      = np.max(matrice[0])
+    
     return np.array([x1, y1, x2, y2])
-print("Bounding box : " + str(roi_bbox()))
+
+#////////////////////////////////////////////
+def alea(mini,maxi):
+    """
+    This function generate a number
+
+    Parameters :
+        x: max number
+    Returns :
+        return random number
+    """
+    return random.randint(mini, maxi)
+
+def random_fill_sparse(table:np.array,k:int):
+    """
+    This function fill random cells with X
+
+    Parameters :
+        table: the empty table
+        k: number of 'X'
+    Returns :
+        return array with random cells fill with X
+    """
+    for i in range(k):
+        table[alea(0,table.shape[0]-1),alea(0,table.shape[1]-1)] = 'X'
+
+    return table
+    
+
 
 
 #////////////////////////////////////////////
-def random_fill_sparse():
-    """
-    This function return an array with random X
-    
-    Parameters :
-    Returns : 
-    """
-   
-    k = 0
-    
-    
-    return k
-print("random_fill_sparse : " + str(random_fill_sparse()))
+theTab = [1,2,3,4,5,6,7,8,9]
+print("Average : "            + str(average_above_zero(theTab)))
+'''
+my_list=[0,2,-3]
+average = average_above_zero(my_list)
+print("T1 Average : " + str(average))
 
-
-
+my_list=9
+average = average_above_zero(my_list)
+print("T2 Average : " + str(average))
+'''
+print("Max : "                + str(max_value(theTab)))
+print("Reverse : "            + str(reverse_table(theTab)))
+print("Bounding box : "       + str(roi_bbox(np.zeros([100,100],dtype = float))))
+print("random_fill_sparse : " + str(random_fill_sparse(np.empty([alea(1,10),alea(1,10)], dtype=str), alea(1,10))))
 
 
 
