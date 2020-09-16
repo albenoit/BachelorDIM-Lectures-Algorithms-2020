057a166170f4f8260e671cbe9ab54137e44dad66
Just1bridou <45208166+Just1bridou@users.noreply.github.com>
2020-09-16 09:54:51 +0200

    feat: bounding box + random fillC:/Anaconda3/python.exe

--- assignements/S1_algotools.py
+++ assignements/S1_algotools.py
@@ -1,17 +1,10 @@
 import numpy as np
+import random
 
 tab = [1,9,3,4,5,6,7,8,2]
 
 np_tab = np.zeros((200,200))
 np_tab[70:90,45:55] = np.ones((20,10))
-
-print(np_tab)
-
-np_random = np.array([
-    ['','',''],
-    ['','',''],
-    ['','','']
-])
 
 def average_above_zero(tab):
     """
@@ -29,8 +22,10 @@ def average_above_zero(tab):
         if i > 0:
             som += i
             n += 1
-
-    return som / n
+    if n > 0:
+        return som / n
+    else:
+        raise ValueError('All numbers are <= 0')
 
 def max_value(tab):
     """
@@ -78,29 +73,59 @@ def reverse_table(tab):
     Returns :
         return the reverse table
     """
-    return tab[::-1]
+    for i in range(len(tab)//2):
+        tmp = tab[i]
+        endVal = len(tab)-i-1
+        tab[i] = tab[endVal]
+        tab[endVal] = tmp
+
+    return tab
 
 def bounding_box(np_tab):
     """
-    This function determine coordinates of true values
+    This function determine a bounding box of true values
 
     Parameters :
         np_tab: the numpy array
     Returns :
-        return the coordinates of true values
+        return the bounding box of true values
     """
-    return np.argwhere(np_tab == 1)
+    matrice = np.where(np_tab != 0)
+
+    return np.array([
+        [np.min(matrice[0]),np.max(matrice[0])],
+        [np.min(matrice[1]),np.max(matrice[1])]
+    ])
+
+def alea(x):
+    """
+    This function generate a random number
+
+    Parameters :
+        x: max number
+    Returns :
+        return random nuumber
+    """
+    return random.randint(0, x)
 
 def random_array_fill(array,k):
-    for i in range(k):
-        print(array.shape[0])
-        print(array.shape[1])
+    """
+    This function fill random cells with X
 
-    #return array
+    Parameters :
+        array: the empty array
+        k: number of X
+    Returns :
+        return array with random cells
+    """
+    for i in range(k):
+        array[alea(array.shape[0]-1),alea(array.shape[1]-1)] = 'X'
+
+    return array
 
 print(average_above_zero(tab))
 print(max_value(tab))
 print(get_index_max_value(tab))
 print(reverse_table(tab))
 print(bounding_box(np_tab))
-print(random_array_fill(np_random,1))
+print(random_array_fill(np.empty([100,100], dtype=str),50))
