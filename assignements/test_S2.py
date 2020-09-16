6d271561fe6e7aeb441878a6f9693ad85433cc70
Lucas Encrenaz <lucas.encrenaz@yahoo.fr>
2020-09-16 16:54:07 +0200

    Test1

--- assignements/test_S2.py
+++ assignements/test_S2.py
@@ -1,6 +1,8 @@
 import S1_algotools as tobetested
 import pytest
 
+list = [8, 15, 14, 12, 6, 18, 10, 2, 20]
+
 def test_myaddition_integers_1():
     assert tobetested.my_addition(10,2) == 12
 
@@ -10,3 +12,9 @@ def test_myaddition_integers_2():
 def test_myaddition_wrong_input():
     with pytest.raises(TypeError):
         tobetested.my_addition('a',2)
+
+def test_average_above_zero_divisionByZero():
+    if n != 0 :
+        moy = som/n
+    else :
+        raise ZeroDivisionError()
