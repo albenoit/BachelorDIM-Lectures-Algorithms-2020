b0acf5707d76c8fabd5313fabcc058128fe5bcf3
Enzo LENTZY <lentzye@iut-acy.local>
2020-09-16 15:21:54 +0200

    test unitaire v4

--- test_algo.py
+++ test_algo.py
@@ -1,13 +1,23 @@
 import S1_algotools as algo 
 
-tab=[1,2,3,4,5]
+tab_positive=[1,2,3,4,5]
+tab_negative=[-1,-2,-3,-4,-5]
+boolean_matrix=np.zeros((12,10),dtype=bool)
+for c in range (7,9):
+    for l in range (4,9):
+        boolean_matrix[l,c]=1
 
-def test_average():
-    assert algo.average(tab)==3
+
+def test_average_1():
+    assert algo.average(tab_positive)==3
+
+def test_average_2():
+    assert algo.average(tab_negative)=='no positive value found'
 
 def test_max_value():
-    assert algo.max_value(tab)==5
+    assert algo.max_value(tab_positive)==5
 
 def test_reverse_table():
-    assert algo.reverse_table(tab) == [5,4,3,2,1]
+    assert algo.reverse_table(tab_positive) == [5,4,3,2,1]
+
 
