937b96f85cd44b5ea0d925e58961d2e4fead68cd
Carl FUCHS <fuchsca@iut-acy.local>
2020-09-16 15:25:33 +0200

    test_max_value working

--- test_S1_Algotools.py
+++ test_S1_Algotools.py
@@ -10,3 +10,9 @@ import S1_algotools as S1
 def test_alea_integers():
     assert S1.alea(5) == 4
     
+
+def test_max_value():
+    mytab = [1,58,67,1054]
+    assert S1.max_value(mytab) == 1054
+    
+    
