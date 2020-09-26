00480682fab931da43085c948b3f56f24c1be7c7
Fabien Vauclin <fabien.vauclin@outlook.fr>
2020-09-26 22:08:20 +0200

    Add Shuffle function (in progress)

---
+++ assignements/S1_dice.py
@@ -0,0 +1,15 @@
+"""
+@author: fabien-v
+"""
+
+# Dice Game
+
+def shuffle(list):
+
+    import random
+    user_score = 0
+    computer_score = 0
+    min = 1
+    max = 6
+    
+    print(random.randint(min, max))
