6fad2736b0d02765121582cff13034b618d9eeef
Antoine BOUVARD <bouvaran@iut-acy.local>
2020-09-29 11:25:41 +0200

    finish invert AND pytest

--- assignements/S3_imgproc_tools.py
+++ assignements/S3_imgproc_tools.py
@@ -11,7 +11,16 @@ img_gray=cv2.imread('imgAlgoPyth.jpg',0)
 img_bgr=cv2.imread('imgAlgoPyth.jpg',1)
 
 
-def invert_colors_manual(input_img):
+def invert_colors_manual(input_img:np.ndarray):
+    """Test si pas typer
+    if input_img is None:
+        raise ValueError('Img not found')
+    if not(isinstance(input_img,np.ndarray)):
+        raise TypeError('Img not uint8')
+    """
+    if input_img.dtype != np.dtype(np.uint8):
+        raise TypeError('Img not uint8')
+        
     """ Une façon de le faire en détail mais pas opti"""
     newImg=np.zeros(input_img.shape, dtype=np.uint8)
     for row in range(input_img.shape[0]):
@@ -21,13 +30,32 @@ def invert_colors_manual(input_img):
     return newImg
 
 
-def invert_colors_numpy(input_img):
+def invert_colors_numpy(input_img:np.ndarray):
+    """Test si pas typer
+    if input_img is None:
+        raise ValueError('Img not found')
+    if not(isinstance(input_img,np.ndarray)):
+        raise TypeError('Img not uint8')
+    """
+    if input_img.dtype != np.dtype(np.uint8):
+        raise TypeError('Img not uint8')
+        
     newImg = 255-input_img
     return newImg
 
 
-def invert_colors_opencv(input_img):
+def invert_colors_opencv(input_img:np.ndarray):
+    """Test si pas typer
+    if input_img is None:
+        raise ValueError('Img not found')
+    if not(isinstance(input_img,np.ndarray)):
+        raise TypeError('Img not uint8')
+    """
+    if input_img.dtype != np.dtype(np.uint8):
+        raise TypeError('Img not uint8')
+        
     return ~input_img
+
 
 
 
