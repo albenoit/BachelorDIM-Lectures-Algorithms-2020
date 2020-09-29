8b4eaf726c117357587040a51fba86e130c344ff
Lucas Encrenaz <lucas.encrenaz@yahoo.fr>
2020-09-29 10:42:53 +0200

    Exo1 finish

--- assignements/S3_imgproc_tools.py
+++ assignements/S3_imgproc_tools.py
@@ -11,20 +11,14 @@ img=cv2.imread('P:/DIM/ALGO/BachelorDIM-Lectures-Algorithms-2020/assignements/my
 # cv2.imshow("BGR image", img_bgr)
 # cv2.waitKey()
 
-img_out = np.zeros(img.shape, dtype=np.uint8)
-img_out = 255-img
 
-# for row in range (img.shape[0]):
-#     for col in range (img.shape[1]):
-#         for channel in range (img.shape[2]):
-#             img_out[row, col, channel]=255-img[row, col, channel]
 
-cv2.imshow('img_out', img_out)
-cv2.waitKey()
 
-# def invert_colors_manual(img):
-#     for i in range(img.shape[0]):
-#         for j in range(img.shape[1]):
-#             print(img[i,j])
-#     return print(img.shape)
-# invert_colors_manual(img)
+def invert_colors_manual(img):
+    img_out = np.zeros(img.shape, dtype=np.uint8)
+    img_out = 255-img
+    cv2.imshow('img_out', img_out)
+    cv2.waitKey()
+    return print(img_out.shape)
+
+invert_colors_manual(img)
