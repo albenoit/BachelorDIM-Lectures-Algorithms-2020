94ded10652a26a47a5098646017acd6acfa89d98
GF Création Web <skeul73@live.fr>
2020-09-29 11:41:30 +0200

    Add function documentation

--- assignements/S3_imgproc_tools.py
+++ assignements/S3_imgproc_tools.py
@@ -1,10 +1,17 @@
 import cv2
 import numpy as np
-img=cv2.imread("iuCkR.jpg")
+img=cv2.imread("BachelorDIM-Lectures-Algorithms-2020/assignements/iuCkR.jpg")
 img_gray=cv2.imread("BachelorDIM-Lectures-Algorithms-2020/assignements/iuCkR.jpg", 0)
 img_bgr=cv2.imread("BachelorDIM-Lectures-Algorithms-2020/assignements/iuCkR.jpg", 1)
 
-def invert_gray_lvl_image(img):
+def invert_gray_lvl_image(img : np.ndarray):
+    '''
+    Function to invert image colors with highest CPU demanding processing
+    Parameters :
+        img: image nd array
+    Returns :
+        imgRet image inverted
+    '''
     img_out=np.zeros(img.shape, dtype=np.uint8)
     for row in range(img.shape[0]):
         for col in range(img.shape[1]):
@@ -12,18 +19,34 @@ def invert_gray_lvl_image(img):
                 img_out[row, col, channel]=255-img[row, col, channel]
     return img_out
 
-def invert_gray_lvl_image_light(img):
+def invert_gray_lvl_image_light(img : np.ndarray):
+    '''
+    Function to invert image colors with lower CPU demanding processing
+    Parameters :
+        img: image nd array
+    Returns :
+        imgRet image inverted
+    '''
     # imgRet=np.zeros(img.shape, dtype=np.uint8)
-    if img is None:
-        raise ValueError('expected an uint8 nd array')
-    if not(isinstance(img, np.ndarray)):
-        raise TypeError('expected an nd array')
+    # if img is None:
+    #     raise ValueError('expected an uint8 nd array')
+    # if not(isinstance(img, np.ndarray)):
+    #     raise TypeError('expected an nd array')
     if img.dtype!=np.dtype(np.uint8):
         raise TypeError('expected uint8 typed nd array')
 
     imgRet = 255-img
     return imgRet
 
+def invert_color_opencv(img : np.ndarray):
+    '''
+    Function to invert image colors with opencv method
+    Parameters :
+        img: image nd array
+    Returns :
+        imgRet image inverted
+    '''
+    return ~img
 # stream=cv2.VideoCapture(0)
 
 
@@ -32,7 +55,7 @@ def invert_gray_lvl_image_light(img):
 #     cv2.imshow("Inverted image", invert_gray_lvl_image_light(imgStream))
 #     cv2.waitKey(1)
 
-# cv2.imshow("test", invert_gray_lvl_imageLight(img))
+# cv2.imshow("test", invert_color_opencv(img))
 # cv2.waitKey()
 
 #display matrix
