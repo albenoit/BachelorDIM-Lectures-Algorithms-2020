216f0b82e7c9d5d18e01fc6a730fc9d93f6e7d6a
Valentin Tapie <valentin.tapie@etu.univ-smb.fr>
2020-09-29 10:04:08 +0200

    invert_colors_opencv_numpy

--- assignements/S3_imgproc_tools.py
+++ assignements/S3_imgproc_tools.py
@@ -26,7 +26,7 @@ def invert_colors_manual(input_img):
        
     return img_out
 
-def invert_colors_numpy(input_img):
+def invert_colors_manualv2(input_img):
     '''
     
     This function reverse the color of an image 
@@ -37,10 +37,31 @@ def invert_colors_numpy(input_img):
     '''
     return 255-input_img
 
+def invert_colors_numpy(input_img):
+    '''
+    
+    This function reverse the color of an image 
+    Parameters:
+        input_img: an image
+    Returns: a reversed image 
+    
+    '''
+    return np.invert(input_img)
+
+def invert_colors_opencv(input_img):
+    '''
+    
+    This function reverse the color of an image 
+    Parameters:
+        input_img: an image
+    Returns: a reversed image 
+    
+    '''
+    return cv2.bitwise_not(input_img)
 
 img_gray=cv2.imread("testimg.jpg",0)
 img_bgr=cv2.imread("testimg.jpg",1)
-img_bgr_reversed = invert_colors_numpy(cv2.imread("testimg.jpg",1))
+img_bgr_reversed = invert_colors_opencv(cv2.imread("testimg.jpg",1))
 
 cv2.imshow("Gray levels image", img_gray)
 cv2.imshow("BGR image", img_bgr)
