49a8fef6939f00fcc9054fa59df1beca4f999510
Charlotte DER-BAGHDASSARIAN <derbaghc@iut-acy.local>
2020-09-29 10:14:08 +0200

    all invert_image functions

--- assignements/S3_imgproc_tools.py
+++ assignements/S3_imgproc_tools.py
@@ -21,28 +21,47 @@ print(img_rgb[1, 1, 2])
 #cv2.waitKey()
 
 
-'''
-Fonction qui inverse les couleurs d'une image. Si elle est en RGB -> nuances 
-de gris et inversement
-parameters : input_image
-return : inversed_img
 
-'''
 def invert_colors_manual(input_image):
+    '''
+    Fonction qui inverse les couleurs d'une image (image négative)
+    parameters : input_image
+    return : inversed_img
+    '''
     ##Pire solution pour Python (besoin de beaucoup de ressources)
     inversed_img = np.zeros(input_image.shape, dtype=np.uint8)
-    
-    '''for x in range(input_image.shape[0]):
+    for x in range(input_image.shape[0]):
         for y in range(input_image.shape[1]):
             for color in range(input_image.shape[2]):
                 inversed_img[x, y, color] = 255 - input_image[x, y, color]
-    cv2.imshow('Negative image', inversed_img)'''
+    cv2.imshow('Negative image', inversed_img)
+
+        
+#invert_colors_manual(img_rgb)  
     
-    ## Version optimale
+    
+
+def invert_colors_numpy(input_image):
+    '''
+    Fonction qui inverse les couleurs d'une image (image négative)
+    parameters : input_image
+    return : inversed_img
+    '''
+    inversed_img = np.zeros(input_image.shape, dtype=np.uint8)
     inversed_img = 255 - input_image
     cv2.imshow('Negative image', inversed_img)
-        
-        
-        
-   
-invert_colors_manual(img_rgb)     
+    
+#invert_colors_numpy(img_rgb)
+
+
+def invert_colors_opencv(input_image):
+    '''
+    Fonction qui inverse les couleurs d'une image (image négative)
+    parameters : input_image
+    return : inversed_img
+    '''
+    inversed_img = cv2.bitwise_not(input_image)
+    cv2.imshow('Negative image', inversed_img)
+    
+invert_colors_opencv(img_rgb)
+
