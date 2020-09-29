eb5534b6c7b91f0676eadddd71769bde90963aaf
albentmp <albentmp@iut-acy.local>
2020-09-29 10:03:01 +0200

    'restruct'

--- Session2/S3_imgproc_tools.py
+++ Session2/S3_imgproc_tools.py
@@ -12,22 +12,13 @@ import numpy as np
 
 #cv2.imshow('Titre',img_gray)
 #cv2.waitKey()
-
-'''
-This function invert the colours of the image using the '255 - img' trick,
-the optimal solution from a python perspective
-@param : input_img (ndarray) is an image
-@return : a ndarray, the image with inverted colours
-'''
-def invert_colors_manual(input_img):
-    return 255 - input_img
     
 '''
 This function invert the colours of the image
 @param : input_img (ndarray) is an image
 @return : a ndarray, the image with inverted colours
 '''
-def invert_colors_manual2(input_img):
+def invert_colors_manual(input_img):
     img_out = np.zeros(img.shape, dtype = np.uint8) #On créer une image vide
     
     #On parcours tous les pixels
@@ -36,6 +27,15 @@ def invert_colors_manual2(input_img):
             for channel in range(img.shape[2]): #Filtre (1: Bleu; 2: Vert; 3: Rouge)
                 img_out[row, col, channel] = 255 - input_img[row, col, channel] #On inverse la couleur
     return img_out
+
+'''
+This function invert the colours of the image using the '255 - img' trick,
+the optimal solution from a python perspective
+@param : input_img (ndarray) is an image
+@return : a ndarray, the image with inverted colours
+'''
+def invert_colors_manual_numpy(input_img):
+    return 255 - input_img
             
 #Test:  
 img_gray = cv2.imread('C:/Users/TEMP/Desktop/algo/img.jpg',0)
