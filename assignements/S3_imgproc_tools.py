667fc09cf796a9ca9897a1ce64100b2fa381e412
Valentin Tapie <valentin.tapie@etu.univ-smb.fr>
2020-09-29 09:52:37 +0200

    invert_colors_2

---
+++ assignements/S3_imgproc_tools.py
@@ -0,0 +1,49 @@
+# -*- coding: utf-8 -*-
+"""
+Éditeur de Spyder
+
+Ceci est un script temporaire.
+"""
+import cv2
+import numpy as np
+
+
+
+def invert_colors_manual(input_img):
+    '''
+    
+    This function reverse the color of an image 
+    Parameters:
+        input_img: an image
+    Returns: a reversed image 
+    
+    '''
+    img_out = np.zeros(input_img.shape, dtype=np.uint8)
+    for row in range(input_img.shape[0]):
+       for col in range(input_img.shape[1]):      
+           for channel in range(input_img.shape[2]):
+               img_out[row,col,channel]=255-input_img[row,col,channel]
+       
+    return img_out
+
+def invert_colors_numpy(input_img):
+    '''
+    
+    This function reverse the color of an image 
+    Parameters:
+        input_img: an image
+    Returns: a reversed image 
+    
+    '''
+    return 255-input_img
+
+
+img_gray=cv2.imread("testimg.jpg",0)
+img_bgr=cv2.imread("testimg.jpg",1)
+img_bgr_reversed = invert_colors_numpy(cv2.imread("testimg.jpg",1))
+
+cv2.imshow("Gray levels image", img_gray)
+cv2.imshow("BGR image", img_bgr)
+cv2.imshow("BGR image inverted", img_bgr_reversed)
+cv2.waitKey()
+
