ae06ee7b73c7b566e8d844f7489a2108a8f2bb6e
Charlotte DER-BAGHDASSARIAN <derbaghc@iut-acy.local>
2020-09-29 11:57:26 +0200

    init functions threshold

--- assignements/S3_imgproc_tools.py
+++ assignements/S3_imgproc_tools.py
@@ -14,18 +14,36 @@ import numpy as np
 img_gray=cv2.imread('image_test_little.jpg', 0)
 img_rgb=cv2.imread('image_test_little.jpg', 1)
 
-print("Gray levels image shape  - " + str(img_gray.shape ))
-print("RGB levels image shape  - " + str(img_rgb.shape ))
-
-print(img_rgb[1, 1, 2])
+#print("Gray levels image shape  - " + str(img_gray.shape ))
+#print("RGB levels image shape  - " + str(img_rgb.shape ))+
+#print(img_rgb[1, 1, 2])
 
 #cv2.imshow('Gray levels image', img_gray)
 #cv2.imshow('RGB levels image', img_rgb)
 #cv2.waitKey()
 
+# =============================================================================
+# Déclencheurs d'erreurs
+# =============================================================================
+def type_errors(img):
+    '''
+    Fonction qui renvoie des erreurs si le type n'est pas bon
+    parameters : img
+    return : 
+    '''
+    ##testé implicitement quand on init le paramètre
+    '''
+    if img is None:
+        raise ValueError('expected an uint8 nd array')
+    if not(isinstance(img, np.ndarray)):
+        raise TypeError('expected a nd array')
+    '''
+    if img.dtype!=np.dtype(np.uint8):
+        raise TypeError('expected uint8 typed nd array')
+        
 
 # =============================================================================
-# Fonctions et déclarations des fonctions
+# Fonctions d'inversion et déclarations
 # =============================================================================
 def invert_colors_manual(input_image):
     '''
@@ -34,6 +52,7 @@ def invert_colors_manual(input_image):
     return : inverted_img
     '''
     ##Pire solution pour Python (besoin de beaucoup de ressources)
+    type_errors(input_image)
     inverted_img = np.zeros(input_image.shape, dtype=np.uint8)
     for x in range(input_image.shape[0]):
         for y in range(input_image.shape[1]):
@@ -49,6 +68,7 @@ def invert_colors_numpy(input_image):
     parameters : input_image
     return : inverted_img
     '''
+    type_errors(input_image)
     inverted_img = 255 - input_image
     return inverted_img
 
@@ -60,10 +80,40 @@ def invert_colors_opencv(input_image):
     parameters : input_image
     return : inversed_img
     '''
+    type_errors(input_image)
     inverted_img = cv2.bitwise_not(input_image)
     return inverted_img
-    
-img_test = invert_colors_manual(img_rgb)
-cv2.imshow('Negative img', img_test)
+
+#img_test = invert_colors_manual(img_rgb)
+#cv2.imshow('Negative img', img_test)
+#cv2.waitKey()
+
+# =============================================================================
+# Fonctions de seuil
+# =============================================================================
+def threshold_image_manual(input_img):
+    '''
+    Fonction qui renvoie une image binaire : on définit un seuil et quand on 
+    passe au dessus de ce seuil -> tout passe en blanc et inversement quand on 
+    passe en dessous du seuil -> tout passe en noir
+    parameters : input_image
+    return : output_image
+    ''' 
+    type_errors(input_img)
+    threshold = 128
+    for row in range(input_img.shape[0]):
+        for col in range(input_img.shape[1]):
+            for color in range(input_img.shape[2]):
+                print(input_img.shape[2])
+                if color > threshold:
+                    input_img[color] = 255
+                if color < threshold:
+                    input_img[color] = 0 
+    return input_img
+
+img_test = threshold_image_manual(img_rgb)
+cv2.imshow('Threshold img', img_test)
 cv2.waitKey()
 
+
+    
