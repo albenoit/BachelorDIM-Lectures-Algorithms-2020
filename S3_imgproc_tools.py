3662549d4e82e9314619321ff0ba5ffb1b8ce3a6
Carl FUCHS <fuchsca@iut-acy.local>
2020-09-29 09:05:54 +0200

    test algo1

--- S3_imgproc_tools.py
+++ S3_imgproc_tools.py
@@ -5,3 +5,15 @@ Created on Tue Sep 29 08:48:56 2020
 @author: fuchsca
 """
 
+import cv2
+import numpy as np
+
+img_gray=cv2.imread('fond-gris.jpg',0)
+img_bgr=cv2.imread('voiture.jpg',1)
+
+print("Gray levels image shape ="+ str(img_gray.shape))
+print("BGR image shape= "+str(img_bgr.shape))
+
+cv2.imshow("gra levels image",img_gray)
+cv2.imshow("BGR image",img_bgr)
+cv2.waitKey()
