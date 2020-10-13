ab5498ef024d9c5f588ac17c7316267dbf2b1f90
Charlotte DER-BAGHDASSARIAN <derbaghc@iut-acy.local>
2020-10-13 13:53:00 +0200

    init gitignore

---
+++ assignements/Session4/simple_queue_publish.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Oct 13 13:47:52 2020
+
+@author: derbaghc
+"""
+
---
+++ assignements/Session4/simple_queue_read.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Oct 13 13:48:25 2020
+
+@author: derbaghc
+"""
+
