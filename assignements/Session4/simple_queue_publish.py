ca0408ee31d1da9bc55257200e6e7e6ad0444e21
PLUME <lucas.ru74@gmail.com>
2020-10-13 15:44:36 +0200

    queue_03

--- assignements/Session4/simple_queue_publish.py
+++ assignements/Session4/simple_queue_publish.py
@@ -1,20 +1,6 @@
-import mykey
-import pika
-import os
+def simple_queue_publish(channel, connection):    
+    channel.basic_publish(exchange='',
+                          routing_key='hello',
+                          body='Hello World!')
 
-# Configure a connexion to a remote RabbitMQ instance:
-amqp_url= mykey.cloudlink
-url = os.environ.get('CLOUDAMQP_URL',amqp_url)
-params = pika.URLParameters(url)
-params.socket_timeout = 5
-#initiate the connexion
-connection = pika.BlockingConnection(params) # Connect to CloudAMQP
-
-channel = connection.channel()
-channel.queue_declare(queue='hello')
-
-channel.basic_publish(exchange='',
-                    routing_key='hello',
-                    body='Hello world!')
-print(" [x] Sent 'Hello World!'")
-connection.close()
+    print(" [x] Sent 'Hello World!'")
