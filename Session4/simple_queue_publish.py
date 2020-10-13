9eb612a38ea3b03a0928d4a42480e0a3126465b3
vcouttmp <vcouttmp@iut-acy.local>
2020-10-13 14:20:16 +0200

    Ajout simple_queue_publish.py

---
+++ Session4/simple_queue_publish.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Oct 13 13:45:21 2020
+
+@author: vcouttmp
+"""
+
+import pika
+import mykeys
+
+amqpurl = mykeys.cloudamqplink
+
+connection = pika.BlockingConnection(pika.URLParameters(amqpurl))
+channel = connection.channel()
+channel.queue_declare(queue='hello')
+
+channel.basic_publish(exchange='',
+                      routing_key='hello',
+                      body='Hello World!')
+print(" [x] Sent 'Hello World!'")
+connection.close()
