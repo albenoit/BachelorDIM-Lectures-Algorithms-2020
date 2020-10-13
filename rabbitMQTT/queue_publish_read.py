fac26979a2dbdba52b6ad773e704333b9ac16530
Carl FUCHS <fuchsca@iut-acy.local>
2020-10-13 16:16:56 +0200

    counter/formating for message reception

--- rabbitMQTT/queue_publish_read.py
+++ rabbitMQTT/queue_publish_read.py
@@ -1,14 +1,63 @@
 # -*- coding: utf-8 -*-
 #!/usr/bin/en python
 import pika
+import os
+import config
 
 """
 Created on Tue Oct 13 13:55:37 2020
 
 @author: fuchsca
 """
+#Connecting
 
+url = os.environ.get('CLOUDAMQP_URL',config.amqp_url)
+params = pika.URLParameters(url)
+params.socket_timeout = 5
 
-connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
+connection = pika.BlockingConnection(params) # Connect to CloudAMQPchannel = connection.channel()
 channel = connection.channel()
-channel.queue_declare(queue='hello')
+
+#Sending
+
+nomMessage = "MonMessage"
+corpsmessage = "je mange un poisson"
+channel.queue_declare(queue=nomMessage)
+channel.basic_publish(exchange='',
+                      routing_key=nomMessage,
+                      body=corpsmessage)
+print(" [X] Sent " + corpsmessage)
+
+#Reception
+counter = 0
+def callback(ch, method, properties,body) :
+    
+    
+    global counter
+    counter = counter + 1
+    
+    print(" [X] Received %r" %body + "il y a eu %r messages" %counter)
+    
+channel.basic_consume(queue=nomMessage,
+                      on_message_callback=callback,
+                      auto_ack=True)
+
+#res = channel.queue_declare(
+#        callback=on_callback,
+#        queue=nomMessage,
+#        durable=True,
+#        exclusive=False,
+#        auto_delete=False,
+#        passive=True)
+#
+#print ("Messages in queue %r" (res.method.message_count))
+
+print("il y a eu %r messages" %counter)
+print(' [*] Waiting for messages. To Exit press CTRL + C')
+channel.start_consuming()
+
+connection.close()
+
+
+
+
