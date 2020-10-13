972fc44f28ff1d77bf782cef85fce26f59451610
Nathan CHARVIN <charvinn@iut-acy.local>
2020-10-13 16:07:38 +0200

    Add for to test multi send messages

--- simple_queue_publish.py
+++ simple_queue_publish.py
@@ -9,6 +9,11 @@ params.socket_timeout = 5
 connection = pika.BlockingConnection(params) # Connect to CloudAMQP
 channel = connection.channel() 
 channel.queue_declare(queue='hello')
-channel.basic_publish(exchange='',
-                      routing_key='hello',
-                      body="Hello World")
+i=0
+for i in range(0, 10):
+    i+=1
+    channel.basic_publish(exchange='',
+                          routing_key='hello',
+                          body="Hello World - "+str(i),
+                          properties=pika.BasicProperties(
+                                  delivery_mode=2))
