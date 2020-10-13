625e171016700fbdb0c0d03cc24db5346b25fa1a
Yoan <yoan-rouleau@live.fr>
2020-10-13 16:05:27 +0200

    Counter + queue_publish_read

---
+++ assignements/queue_publish_read.py
@@ -0,0 +1,36 @@
+import argparse
+import pika
+import mykeys
+
+connection = pika.BlockingConnection(pika.URLParameters(mykeys.cloudamqplink))
+channel = connection.channel()
+channel.queue_declare(queue='hellothere')
+
+parser = argparse.ArgumentParser(description='How to')
+parser.add_argument("-read", action='store_true')
+FLAGS = parser.parse_args()
+
+def read():
+    count = 0
+    def callback(ch, method, properties, body):
+        global count
+        count += 1
+        print("Count: " + str(count) + " ; [x] Received %r" % body)
+
+    channel.basic_consume(queue='hellothere',
+                          on_message_callback=callback,
+                          auto_ack=True)
+    print(' [x] Waiting for messages. To exit, press CTRL+C')
+    channel.start_consuming()
+
+def publish():
+    channel.basic_publish(exchange='',
+                          routing_key='hellothere',
+                          body='Hello World!')
+    print(" [x] Sent 'Hello World!'")
+    connection.close()
+
+if FLAGS.read:
+    read()
+else:
+    publish()
--- assignements/simple_queue_read.py
+++ assignements/simple_queue_read.py
@@ -6,12 +6,10 @@ channel = connection.channel()
 channel.queue_declare(queue='hellothere')
 
 count = 0
-
 def callback(ch, method, properties, body):
     global count
     count += 1
     print("Count: " + str(count) + " ; [x] Received %r" % body)
-
 
 channel.basic_consume(queue='hellothere',
                       on_message_callback=callback,
