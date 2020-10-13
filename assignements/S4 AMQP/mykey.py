157dde3849d32d7e28dee06584d75a805bc8e1de
Guillian VIBERT-VALLET <vibertvg@iut-acy.local>
2020-10-13 14:27:10 +0200

    Add simple queue publish

---
+++ assignements/S4 AMQP/mykey.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Oct 13 14:23:38 2020
+
+@author: vibertvg
+"""
+cloudlink = 'amqps://lntaqknb:1ML9V3bCpi1MxDMWMZUv5Uqu-2HK9X4Q@chinook.rmq.cloudamqp.com/lntaqknb'
---
+++ assignements/S4 AMQP/simple_queue_publish.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Oct 13 14:03:45 2020
+
+@author: vibertvg
+"""
+
+import mykey
+import pika
+import os
+
+# Configure a connexion to a remote RabbitMQ instance:
+amqp_url= mykey.cloudlink
+url = os.environ.get('CLOUDAMQP_URL',amqp_url)
+params = pika.URLParameters(url)
+params.socket_timeout = 5
+#initiate the connexion
+connection = pika.BlockingConnection(params) # Connect to CloudAMQP
+
+channel = connection.channel()
+channel.queue_declare(queue='hello')
+
+channel.basic_publish(exchange='',
+                    routing_key='hello',
+                    body='Hello world!')
+print(" [x] Sent 'Hello World!'")
+connection.close()
