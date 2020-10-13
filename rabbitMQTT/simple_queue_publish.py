54430974460edfad01134d2bd5591a4b5dfd0b56
Carl FUCHS <fuchsca@iut-acy.local>
2020-10-13 14:38:51 +0200

    simple publish working

--- rabbitMQTT/simple_queue_publish.py
+++ rabbitMQTT/simple_queue_publish.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 #!/usr/bin/en python
 import pika
+import os
+import config
 
 """
 Created on Tue Oct 13 13:47:56 2020
@@ -8,11 +10,18 @@ Created on Tue Oct 13 13:47:56 2020
 @author: fuchsca
 """
 
-connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
+
+url = os.environ.get('CLOUDAMQP_URL',config.amqp_url)
+params = pika.URLParameters(url)
+params.socket_timeout = 5
+
+connection = pika.BlockingConnection(params) # Connect to CloudAMQPchannel = connection.channel()
 channel = connection.channel()
 channel.queue_declare(queue='hello')
 channel.basic_publish(exchange='',
-                              rounting_key='hello',
-                              body='Hello World!')
+                      routing_key='hello',
+                      body='Hello World!')
 print(" [X] Sent 'Hello World!'")
+
+
 connection.close()
