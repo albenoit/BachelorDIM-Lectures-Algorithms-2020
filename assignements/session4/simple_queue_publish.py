53f621bec4d3fdf100e7e122e8642cbc597f5b84
Lea PORTIER <portierl@iut-acy.local>
2020-10-13 15:22:16 +0200

    fichier queue publish

--- assignements/session4/simple_queue_publish.py
+++ assignements/session4/simple_queue_publish.py
@@ -1,1 +1,16 @@
-import pika;
+import pika
+import config
+import os
+
+url = os.environ.get('CLOUDAMPQ_url', config.keyAMQP)
+params = pika.URLParameters(url)
+params.socket_timeout = 5
+
+connection = pika.BlockingConnection(params)
+channel = connection.channel()
+channel.queue_declare(queue='hello')
+
+
+channel.basic_publish(exchange='', routing_key='hello', body='hello world')
+print ("[x] sent hello world")
+connection.close()
