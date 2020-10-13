d4bfbd41bb53f2ed415afae47fc3a93849b1fa82
vcouttmp <vcouttmp@iut-acy.local>
2020-10-13 16:02:49 +0200

    Ajout queue_publish_read.py

---
+++ Session4/queue_publish_read.py
@@ -0,0 +1,55 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Oct 13 14:34:50 2020
+
+@author: vcouttmp
+"""
+
+import argparse
+import pika
+import mykeys
+
+amqpurl = mykeys.cloudamqplink
+
+count = 0
+
+def publish(message):
+    connection = pika.BlockingConnection(pika.URLParameters(amqpurl))
+    channel = connection.channel()
+    channel.queue_declare(queue='hello')
+
+    channel.basic_publish(exchange='',
+                      routing_key='hello',
+                      body=message)
+    print(" [x] Sent 'Hello World!'")
+    connection.close()
+  
+def callback(ch, method, properties, body):
+    print (" [x] Received %r" % body)
+    global count
+    count+=1
+    print ('message lu : ', count)
+    
+def read():    
+    connection = pika.BlockingConnection(pika.URLParameters(amqpurl))
+    channel = connection.channel()
+    channel.queue_declare(queue='hello')
+
+    channel.basic_consume(queue='hello',
+                          on_message_callback = callback,
+                          auto_ack=True)
+    
+    print(' [*] Waiting for messages. To exit press CTRL+C')
+    channel.start_consuming()
+
+
+
+parser = argparse.ArgumentParser(description= "How to")
+parser.add_argument('-read', action='store_true')
+parser.add_argument('-m')
+FLAGS = parser.parse_args()
+
+if(FLAGS.read):
+    read()
+else:
+    publish(FLAGS.m)
