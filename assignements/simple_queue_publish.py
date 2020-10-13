66d36edfbc8c65c5aa9aeffb0b6fc17f10e5b223
Justin ANTHOINE-MILHOMME <anthoinj@iut-acy.local>
2020-10-13 14:26:34 +0200

    fix: publish

--- assignements/simple_queue_publish.py
+++ assignements/simple_queue_publish.py
@@ -3,10 +3,10 @@ from keys import Keys
 key = Keys()
 AMQP_url = key.getAMQP_URL()
 
-connection = pika.BlockingConnection(pika.ConnectionParameters(host=AMQP_url, port=1883))
+connection = pika.BlockingConnection(pika.URLParameters(AMQP_url))
 channel = connection.channel()
-channel.queue_declare(queue='hello')
+channel.queue_declare(queue='presentation')
 
-channel.basic_publish(exchange='', routing_key='hello', body='Hello World !')
+channel.basic_publish(exchange='', routing_key='presentation', body='Hello World !')
 print(" [x] Sent 'Hello World!'")
 connection.close()
