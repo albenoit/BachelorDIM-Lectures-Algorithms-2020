1e02dc21de20c155a86d340d74fab26bcff487b6
Nathan CHARVIN <charvinn@iut-acy.local>
2020-10-13 14:34:55 +0200

    Publish

--- simple_queue_publish.py
+++ simple_queue_publish.py
@@ -1,13 +1,16 @@
 import pika
-connection = pika .BlockingConnection(pika.ConnectionParameters(host='localhost'))
+import os
+import config
+
+amqp_url=config.AMQP_URL
+url = os.environ.get('CLOUDAMQP_URL',amqp_url)
+params = pika.URLParameters(url)
+params.socket_timeout = 5
+connection = pika.BlockingConnection(params) # Connect to CloudAMQP
 channel = connection.channel() 
 channel.queue_declare(queue='hello')
-
+channel.basic_publish(exchange='',
+                      routing_key='hello',
+                      body="Hello World")
 def callback(ch, method, properties, body):
     print(" [x] Received %r" % body)
-
-channel.basic_consume(queue='hello',
-                       on_message_callback=callback,
-                       auto_ack=True  ) 
-print ( ' [*] Waiting for messages. To exit press CTRL+C') 
-channel.start_consuming()
