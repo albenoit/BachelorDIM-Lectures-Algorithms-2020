3fb7e3f9afe806428fe573865fc9da6c0a26eee5
Baptiste POLLET <polletb@iut-acy.local>
2020-10-13 14:21:35 +0200

    read cloudamqp data OK

--- simple_queue_read.py
+++ simple_queue_read.py
@@ -6,5 +6,23 @@ Created on Tue Oct 13 13:06:29 2020
 """
 
 from decouple import config 
+import pika
+import os
 
 AMQP_URL = config('AMQP_URL')
+
+url = os.environ.get('CLOUDAMQP_URL', AMQP_URL)
+params = pika.URLParameters(url)
+connection = pika.BlockingConnection(params)
+channel = connection.channel()
+channel.queue_declare(queue='hello')
+def callback(ch, method, properties, body):
+  print(" [x] Received " + str(body))
+
+channel.basic_consume('hello',
+                      callback,
+                      auto_ack=True)
+
+print(' [*] Waiting for messages:')
+channel.start_consuming()
+connection.close()
