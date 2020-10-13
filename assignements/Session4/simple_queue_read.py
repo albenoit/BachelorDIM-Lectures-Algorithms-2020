b111c276fc303f9def3c43ca628ae98dc97636c3
PLUME <lucas.ru74@gmail.com>
2020-10-13 14:48:39 +0200

    use a gitignore for security v2

---
+++ assignements/Session4/simple_queue_read.py
@@ -0,0 +1,22 @@
+import mykey
+import pika
+import os
+
+# Configure a connexion to a remote RabbitMQ instance:
+amqp_url= mykey.cloudlink
+url = os.environ.get('CLOUDAMQP_URL',amqp_url)
+params = pika.URLParameters(url)
+params.socket_timeout = 5
+#initiate the connexion
+connection = pika.BlockingConnection(params) # Connect to CloudAMQP
+
+channel = connection.channel()
+channel.queue_declare(queue='hello')
+
+def callback(ch,method,properties, body):
+    print(" [x] Received %r" % body)
+    
+channel.basic_consume(queue='hello',
+                      on_message_callback=callback,auto_ack=True)
+print(' [*] Waiting for messages. To exit press CTRL+C')
+channel.start_consuming()
