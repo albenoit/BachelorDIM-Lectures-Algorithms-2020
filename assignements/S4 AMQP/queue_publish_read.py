a65b30e5920aa02f32ee5dcad4bdd11690eff531
Guillian VIBERT-VALLET <vibertvg@iut-acy.local>
2020-10-20 11:31:53 +0200

    Create new queue and add durability

--- assignements/S4 AMQP/queue_publish_read.py
+++ assignements/S4 AMQP/queue_publish_read.py
@@ -5,33 +5,59 @@ Created on Tue Oct 13 14:43:47 2020
 @author: vibertvg
 """
 
-
-import argparse
-import mykey
+from decouple import config 
+import argparse as argp
 import pika
 import os
 import simple_queue_publish
 import simple_queue_read
 
-parser= argparse.ArgumentParser(description="How to")
+'''
+Use Python-decouple for .env file
+'''
+AMQP_URL = config('AMQP_URL')
+
+'''
+Use argparse to add argument before execution
+'''
+parser = argp.ArgumentParser(description="How to")
 parser.add_argument('-read', action='store_true')
+parser.add_argument('-concurrency', action='store_true')
 flags = parser.parse_args()
 
-#flags.read = True
-
-amqp_url= mykey.cloudlink
-url = os.environ.get('CLOUDAMQP_URL', amqp_url)
+'''
+Connect params to cloudAMQP with Pika
+'''
+url = os.environ.get('CLOUDAMQP_URL', AMQP_URL)
 params = pika.URLParameters(url)
 params.socket_timeout = 5
 connection = pika.BlockingConnection(params)
+
+'''
+Test type channel with argument
+'''
+queueName = "myQueue"
+
 channel = connection.channel()
-channel.queue_declare(queue='hello')
-
-
-#Test pour savoir le type d'exucution à effectuer
-if flags.read:
-    simple_queue_read.simple_queue_read(channel, connection)
+if flags.concurrency and flags.read:
+    channel.queue_declare(queue=queueName, durable=True)
+    channel.basic_qos(prefetch_count=10)
+elif flags.concurrency:
+    channel.queue_declare(queue=queueName, durable=True)
 else:
-    simple_queue_publish.simple_queue_publish(channel, connection)
+    channel.queue_declare(queue=queueName)
+    #print(queue.method.queue)
+
+'''
+Test for argument
+'''
+if flags.read:
+    simple_queue_read.read_queue(channel, queueName)
+else:
+    for i in range(0, 100):
+        simple_queue_publish.publish_queue(channel, queueName)
+    
+
+connection.close()
 
 
