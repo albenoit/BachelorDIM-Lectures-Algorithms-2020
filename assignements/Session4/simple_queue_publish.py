101fd1a13108ef40650beebaaf5c93f19f028351
PLUME <lucas.ru74@gmail.com>
2020-10-20 09:47:23 +0200

    queue_04

--- assignements/Session4/simple_queue_publish.py
+++ assignements/Session4/simple_queue_publish.py
@@ -1,7 +1,12 @@
+import pika
+
 def simple_queue_publish(channel, connection):
     channel.basic_publish(exchange='',
                           routing_key='hello',
-                          body='Hello World!')
+                          body='Hello World!',
+                          properties=pika.BasicProperties(
+                              delivery_mode = 2,
+                          ))
 
     print(" [x] Sent 'Hello World!'")
     connection.close()
--- assignements/Session4/simple_queue_read.py
+++ assignements/Session4/simple_queue_read.py
@@ -1,10 +1,11 @@
 def callback(ch, method, properties, body):
   print(" [x] Received " + str(body))
+  ch.basic_ack(delivery_tag = method.delivery_tag)
 
 
 def simple_queue_read(channel, connection):
     channel.basic_consume('hello',
                       on_message_callback=callback,
-                      auto_ack=True)
+                      auto_ack=False)
     print(' [*] Waiting for messages:')
     channel.start_consuming()
