2949622527a1f0a586cd30bce36577a03d810f2d
GF Création Web <skeul73@live.fr>
2020-10-20 11:48:37 +0200

    Edit fanout mode in new files

---
+++ assignements/Session4/publish_fanout.py
@@ -0,0 +1,36 @@
+import pika
+import config
+import os
+import time
+
+'''
+Function to send message with CLOUDAMPQ
+Parameters :
+    message: the message to sent,
+    concurrency: parameter to set persistent message option
+'''
+#configuration
+url = os.environ.get('CLOUDAMQP_URL', config.amqp_url)
+params = pika.URLParameters(url)
+params.socket_timeout = 5
+
+#connexion
+connection = pika.BlockingConnection(params)
+channel = connection.channel()
+channel.queue_declare(queue='presentation')
+channel.exchange_declare(exchange='posts', exchange_type='fanout')
+
+ti = 1
+while(ti < 50):
+    message = 'Test d\'envoi de message avec fanout exchange mode ==>%r' % ti
+    channel.basic_publish(exchange='posts',
+                    routing_key='',
+                    body=message,
+                    )
+    print((" Sent %r" % message))
+    print((" Sent %reme message" % ti))
+    ti = ti + 1
+    time.sleep(5)
+
+
+connection.close()
