ffc30e03d05b6fc13a7962c3e8259d9850553543
Guillian VIBERT-VALLET <vibertvg@iut-acy.local>
2020-10-20 10:47:33 +0200

    add doxygen synthax

--- assignements/S4 AMQP/simple_queue_publish.py
+++ assignements/S4 AMQP/simple_queue_publish.py
@@ -6,7 +6,15 @@ Created on Tue Oct 13 14:03:45 2020
 """
 
 
-def simple_queue_publish(channel, connection):    
+def simple_queue_publish(channel, connection): 
+    """
+    Function who write the messages and send it to queue
+    Parameters:
+        channel : channel to send
+        Connetion: connect to queue
+    Returns:
+        return sending message hello world
+    """
     channel.basic_publish(exchange='',
                           routing_key='hello',
                           body='Hello World!')
--- assignements/S4 AMQP/simple_queue_read.py
+++ assignements/S4 AMQP/simple_queue_read.py
@@ -7,6 +7,13 @@ Created on Tue Oct 13 14:14:44 2020
 count = 0
 
 def callback(ch, method, properties, body):
+  """
+  Function who write the messages recieved
+  Parameters:
+      body: message recieved
+  Returns:
+      return message recieved in print and the number of message recieved
+  """
   print(" [x] Received " + str(body))
   global count
   count = count +1
@@ -14,6 +21,13 @@ def callback(ch, method, properties, body):
 
 
 def simple_queue_read(channel, connection):
+    """
+    Function who write the messages recieved
+    Parameters:
+        body: message recieved
+    Returns:
+        return message recieved in print and the number of message recieved
+    """
     channel.basic_consume('hello',
                       on_message_callback=callback,
                       auto_ack=True)
