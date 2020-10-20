4aeb6cf0e1ed32052498f98647160be343f6628a
Enzo LENTZY <lentzye@iut-acy.local>
2020-10-20 11:52:02 +0200

    Was modifying simple_queue_read for adding argument on -concurrency so user can choose either queue. It doesn't work yet

--- simple_queue_read.py
+++ simple_queue_read.py
@@ -6,7 +6,7 @@ Created on Tue Oct 13 14:24:16 2020
 """
 import mykeys
 import pika, os
-
+import argparse
 
 url = os.environ.get('CLOUDAMQP_URL', mykeys.cloudampqlink)
 params = pika.URLParameters(url)
@@ -16,10 +16,19 @@ channel.queue_declare(queue='Coucou',durable=True) # Declare a queue
 def callback(ch, method, properties, body):
   print(" [x] Received " + str(body))
 
-channel.basic_consume('Coucou',
+parser = argparse.ArgumentParser(description='How to')
+parser.add_argument('-concurrency', action='store_true')
+flags=parser.parse_args()
+
+if(flags.concurrency):  
+    channel.basic_consume('Coucou',
                       callback,
                       auto_ack=False)
+else :
+    channel.basic_consume('hello',
+                      callback,
+                      auto_ack=True)
 
-print(' [*] Waiting for messages:')
+
 channel.start_consuming()
 connection.close()
