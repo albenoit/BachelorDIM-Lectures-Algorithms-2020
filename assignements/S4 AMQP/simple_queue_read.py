6123436c8ae42012798a13a6502bfb034e2e36ea
Guillian VIBERT-VALLET <vibertvg@iut-acy.local>
2020-10-20 09:36:51 +0200

    add count for recieving messages in simple_queue_read

--- assignements/S4 AMQP/simple_queue_read.py
+++ assignements/S4 AMQP/simple_queue_read.py
@@ -4,9 +4,13 @@ Created on Tue Oct 13 14:14:44 2020
 
 @author: vibertvg
 """
+count = 0
 
 def callback(ch, method, properties, body):
   print(" [x] Received " + str(body))
+  global count
+  count = count +1
+  print(count)
 
 
 def simple_queue_read(channel, connection):
