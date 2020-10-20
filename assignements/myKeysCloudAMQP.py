f9ae8851ee8676d9d118b954563ad6ac65b08262
kceriati <71388352+kceriati@users.noreply.github.com>
2020-10-20 09:49:02 +0200

    Update myKeysCloudAMQP.py

--- assignements/myKeysCloudAMQP.py
+++ assignements/myKeysCloudAMQP.py
@@ -3,6 +3,9 @@
 Created on Tue Oct 13 13:57:16 2020
 
 @author: ceriatik
+
+##TODO##
+Add your AMQP key  here
 """
 
 cloud_amqp_key = 'amqps://omjfrtix:mq5hi4_kJBKAHzwICye02mCF0yDYP59Z@kangaroo.rmq.cloudamqp.com/omjfrtix'
