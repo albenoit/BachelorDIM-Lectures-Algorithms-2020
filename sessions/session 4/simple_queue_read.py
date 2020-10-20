89742d95216761cdeb899a55e760dbfc411b4f21
Loic VIARD-CRETAT <viardcrl@iut-acy.local>
2020-10-20 10:00:18 +0200

    print message position

--- sessions/session 4/simple_queue_read.py
+++ sessions/session 4/simple_queue_read.py
@@ -2,9 +2,11 @@
 import pika, os
 
 def callback(ch, method, properties, body):
-    print(" [x] Received " + str(body))
+    print(" [" + str(method.delivery_tag) + "] Received " + str(body))
 
 def read_queue(channel):
     channel.basic_consume('hello', callback, auto_ack=True)
     print(' [*] Waiting for messages:')
     channel.start_consuming()
+
+
