2aae27cd10463b2e256bff58b015261b218baeb4
corentyndormoy <corentyn.dormoy@gmail.com>
2020-11-03 19:38:38 +0100

    Ajout Dice Game

--- Session1/S1_dice.py
+++ Session1/S1_dice.py
@@ -10,15 +10,51 @@ from random import *
 def shuffle():
     pointA = 0
     pointB = 0
+    tour = 0
+    lON = ['o','n']
+    
     while (pointA <100 and pointB <100):
         de = 0
         temp = 0
+        strUser = ""
+        tour += 1
+
         while (de != 1 and strUser != "n"):
-            de = randint(0,6)
+            de = randint(1,6)
             temp += de
-            input("Score temporaire",temp,".Le garder? (o/n)")
-        if(de != 1):
-            pointA += temp
-        temp = 0
-            
-            
+            print("Dé : ",de)
+
+            if(de != 1):
+                if (tour % 2 != 0):
+                    #Humain
+                    strUser = ""
+                    while(strUser not in lON):
+                        strUser = input("Score temporaire: " + str(temp) + ".Relancer? (o/n)")
+                    if(strUser == 'n'):
+                        pointA += temp
+                else:
+                    #Bot
+                    strUser = "o"
+                    print("Score temporaire: " + str(temp))
+                    if(randint(0,1)):
+                        print("Le bot relance")
+                    else:
+                        strUser = "n"
+                        print("Le bot s'arrête")
+                        pointB += temp
+            else:
+                print("Le dé affiche 1. Vous ne gagnez pas de point.")
+                
+        print("-------------------------")
+        print("Score joueur :" + str(pointA))
+        print("Score bot :" + str(pointB))
+        print("-------------------------")
+
+        
+    if(pointA > 100):
+        print("Humain a gagné")
+    else:
+        print("Bot a gagné")
+
+shuffle()
+
