1224fbd8068365521f59e5382d19efb308470d2e
charlottedrb <c.derbaghdassarian@gmail.com>
2020-11-07 12:42:58 +0100

    bubble sort - questions and function

--- assignements/Session1/S1_algotools.py
+++ assignements/Session1/S1_algotools.py
@@ -235,6 +235,10 @@ input_image[1:3,2:4] = np.ones((2, 3))
 """
 SORTING
 """
+
+"""
+Selective sort
+"""
 """
 a) Illustration du tri par sélection sur 10, 15, 7, 1, 3, 3, 9
     1. On recherche sur 0 - 6 le minimum : c'est 1
@@ -255,29 +259,136 @@ a) Illustration du tri par sélection sur 10, 15, 7, 1, 3, 3, 9
     6. Sur 5 - 6 le minimum : 10 
     Le tableau était déjà trié. Fin. 
 """
-def selection_sort(list):
-    size = len(list)
+
+"""
+Fonction qui trie une liste avec la méthode du tri par sélection.
+param : list_in de type list
+return : list_in
+"""
+def selection_sort(list_in:list):
+    size = len(list_in)
+    permutations = 0
+    comparisons = 0
     for i in range(size-1):
         index_min = i
-        tmp = list[i]
+        tmp = list_in[i]
         for j in range(i+1, size):
-            if(list[index_min] > list[j]):
+            if(list_in[index_min] > list_in[j]):
                 index_min = j
-        list[i], list[index_min] = list[index_min], list[i]
-    return list
+                comparisons += 1
+        # permet d'intervertir deux valeurs
+        list_in[i], list_in[index_min] = list_in[index_min], list_in[i]
+        permutations += 1
+    print("permutations : " +str(permutations))
+    print("comparisons : " + str(comparisons))
+    return list_in
 
-list = [10, 15, 7, 1, 3, 3, 9]
-print(selection_sort(list))
-
-
-
-
-
+#list = [10, 15, 7, 1, 3, 3, 9]
+#list=[alea(100) for i in range(100)]
+#print(selection_sort(list))
 
 """
 b) Oui, le nb d'itérations dépend du contenu. 
 c) Dans ce cas là, 6, donc j'en déduis qu'il en faut le nombre de la taille. 
 d) Ici on a fait 5 permutations. 
-e) Il y a autant de comparaisons qu'il y a de permutations 
-f) Je dirais qu'il a une complexité O(n2)
+e) On a fait 8 comparisons.
+f) Il a une complexité O(n2).
+g) Le nombre de permutations est toujours égal à size-1 mais le nombre de comparaison dépend à mon avis
+   des valeurs du tableau.
 """
+
+"""
+Bubble sort 
+"""
+"""
+a) Illustration du tri par bulles sur 10, 15, 7, 1, 3, 3, 9
+    Premier passage pour mettre le maximum à la fin. 
+        1. On compare sur 0 - 1 le minimum : c'est 10
+        On a 10, 15, 7, 1, 3, 3, 9
+    
+        2. Sur 1 - 2 le minimum : 7
+        On a 10, 7, 15, 1, 3, 3, 9
+    
+        3. Sur 2 - 3 le minimum : 1
+        On a 10, 7, 1, 15, 3, 3, 9
+    
+        4. Sur 3 - 4 le minimum : 3
+        On a 10, 7, 1, 3, 15, 3, 9
+    
+        5. Sur 4 - 5 le minimum : 3
+        On a 10, 7, 1, 3, 3, 15, 9
+    
+        6. Sur 5 - 6 le minimum : 9 
+        On a 10, 7, 1, 3, 3, 9, 15. Le maximum est remonté.  
+    Deuxième passage :
+        1. On compare sur 0 - 1 le minimum : 7
+        On a 7, 10, 1, 3, 3, 9, 15
+    
+        2. Sur 1 - 2 le minimum : 1
+        On a 7, 1, 10, 3, 3, 9, 15
+    
+        3. Sur 2 - 3 le minimum : 3
+        On a 7, 1, 3, 10, 3, 9, 15
+    
+        4. Sur 3 - 4 le minimum : 3
+        On a 7, 1, 3, 3, 10, 9, 15
+    
+        5. Sur 4 - 5 le minimum : 9
+        On a 7, 1, 3, 3, 9, 10, 15. 
+    Troisième passage :
+        1. On compare sur 0 - 1 le minimum : 1
+        On a 1, 7, 3, 3, 9, 10, 15.
+    
+        2. Sur 1 - 2 le minimum : 3
+        On a 1, 3, 7, 3, 9, 10, 15
+    
+        3. Sur 2 - 3 le minimum : 3
+        On a 1, 3, 3, 7, 9, 10, 15
+    
+        4. Sur 3 - 4 le minimum : 7
+        On a 1, 3, 3, 7, 9, 10, 15 
+    Quatrième passage :
+        Le tri passe et voit que la liste est bien triée, il s'arrête.         
+"""
+
+"""
+Fonction qui trie une liste avec la méthode du tri à bulles.
+param : list_in de type list
+return : list_in
+"""
+def bubble_sort(list_in:list):
+    size = len(list_in)
+    iterations = 0
+    permutations = 0
+    comparisons = 0
+    for i in range(size-1):
+        #on met -i car le max est placé on a plus besoin d'y toucher
+        iterations += 1
+        for j in range(0, size-1-i):
+            iterations += 1
+            if(list_in[j] > list_in[j+1]):
+                comparisons += 1
+                #permet d'intervertir deux valeurs
+                list_in[j], list_in[j+1] = list_in[j+1], list_in[j]
+                permutations += 1
+    print("iterations : " + str(iterations))
+    print("comparisons : " + str(comparisons))
+    print("permutations : " + str(permutations))
+    return list_in
+
+#list = [10, 15, 7, 1, 3, 3, 9]
+list=[alea(500) for i in range(500)]
+print(bubble_sort(list))
+
+"""
+b) Oui, plus il y aura de valeurs, et plus il y aura d'itérations pour mettre le max à la fin
+c) Pour la liste de 6 valeurs, on a 27 itérations (6 pour la première boucle et 21 pour la deuxième).
+Cela correspond aux passages de l'illustration ci-dessus
+d) Il y a eu 13 permutations, 2 fois la longueur du tableau moins 1 car on ne permute pas la dernière case, 
+le maximum remonte de lui-même lors du premier passage.
+e) Il y a eu 13 comparisons, pour la même raison que les permutations. 
+f) Je dirais que la complexité de cet algorithme est de 0(n2) 
+g) Le nombre d'itérations correspond à peu près au double de permutations et de comparaisons. 
+Les permutations et les comparaisons sont toujours égales, mais beaucoup plus importantes que le tri par sélection.
+"""
+
