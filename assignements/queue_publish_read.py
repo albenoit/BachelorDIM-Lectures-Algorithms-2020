cb7e9b548f681b6bf80526a944fd2b177787c7c6
Nathan Cuvellier <cuvellier.nathan@gmail.com>
2020-11-09 21:01:33 +0100

    change script in order to emit 1000 persistent messages to the same queue

--- assignements/queue_publish_read.py
+++ assignements/queue_publish_read.py
@@ -8,8 +8,8 @@ Created on Tue Oct 13 15:33:27 2020
 import config, pika, os, argparse, simple_queue_publish, simple_queue_read
 
 parser = argparse.ArgumentParser("How to")
-parser.add_argument("-read",help="read messages", action="store_true")
-parser.add_argument("-concurrency",help="multiple concurrent queue readers", action="store_true")
+parser.add_argument("-read", help="read messages", action="store_true")
+parser.add_argument("-concurrency", help="multiple concurrent queue readers", action="store_true")
 args = parser.parse_args()
 
 url = os.environ.get('CLOUDAMQP_URL', config.amqp_url)
@@ -17,19 +17,18 @@ params = pika.URLParameters(url)
 params.socket_timeout = 5
 connection = pika.BlockingConnection(params)
 
-channel = connection.channel() # start a channel
+channel = connection.channel()  # start a channel
 
 if args.concurrency:
     queue = channel.queue_declare("task_queue", durable=True)
     channel.basic_qos(prefetch_count=1)
 else:
     queue = channel.queue_declare(queue='hello')
-    
-    
+
 if args.read:
     simple_queue_read.read(channel, queue.method.queue)
-else :
-    simple_queue_publish.publish(channel, queue.method.queue)
-    
-    
+else:
+    #  simple_queue_publish.publish(channel, queue.method.queue)
+    simple_queue_publish.publish_thounsand_messages(channel, queue.method.queue)
+
 connection.close()
--- assignements/simple_queue_publish.py
+++ assignements/simple_queue_publish.py
@@ -7,26 +7,34 @@ Created on Tue Oct 13 13:53:05 2020
 import pika
 
 count_publish = 0
+
+
 def publish(channel, queue_name: str):
-    '''
+    """
     publish and show the message write in the body params with incremental number
-    
+
     Parameters:
         channel
+        queue_name
     Return:
         void
-    '''
+    """
     global count_publish
-    count_publish += 1 
-    exchange=''
+    count_publish += 1
+    exchange = ''
     if queue_name == 'task_queue':
-        exchange='logs'
-    
+        exchange = 'logs'
+
     channel.basic_publish(exchange=exchange,
                           routing_key=queue_name,
                           body='Hello Word!',
                           properties=pika.BasicProperties(
-                                  delivery_mode = 2
-                             )
+                              delivery_mode=2
+                          )
                           )
     print(" [" + str(count_publish) + "] Sent 'Hello World !'")
+
+
+def publish_thounsand_messages(channel, queue_name: str):
+    for i in range(0, 1000):
+        publish(channel, queue_name)
