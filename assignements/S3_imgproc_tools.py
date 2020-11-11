10ba6673bbe9e328db9696d24eac1fc6154afb82
Vapie <48350646+Vapie@users.noreply.github.com>
2020-11-11 17:04:20 +0100

    Final Tp3

--- assignements/S3_imgproc_tools.py
+++ assignements/S3_imgproc_tools.py
@@ -4,6 +4,7 @@
 
 Ceci est un script temporaire.
 """
+
 import cv2
 import numpy as np
 
@@ -58,48 +59,92 @@ def invert_colors_opencv(input_img):
     
     '''
     return cv2.bitwise_not(input_img)
-def innv_gray_levels(img:np.ndarray):
-        '''
-        This function reverse the color of an image 
-        Parameters:
-            input_img: an image
-        Returns: a reversed image as ndarray
-        
-        '''
-    
-        """
-        if img is None:
-            raise ValueError ("pas bien g eu none")
-        if not(isinstance(img,np.nparray)):
-            raise ValueError ("pas bien j'ai eu pas lee bon type")
-            """
-        if img.dtype!=np.dtype(np.uint8):
-            raise TypeError ("expected uint8 typed np array")
-        return 255-img
-  
-def threshold(img:np.ndarray):
+
+
+
+
+def img_to_gray(img:np.array):
     """
-    This function apply a threshold on the image given in params 
+    This function take an image and turn it in grayscale
+    Parameters:
+        img: an image
+    Returns:
+        a grayscaled image as a ndarray
+
+    """
+    img_out = np.zeros(img.shape, dtype=np.uint8)
+    for row in range(img.shape[0]):
+        for col in range(img.shape[1]):
+            moy = (int(img[row, col, 0]) +int(img[row, col, 1]) +int(img[row, col, 2]))/3
+            img_out[row, col, 0]= moy
+            img_out[row, col, 1]= moy
+            img_out[row, col, 2] = moy
+    return img_out
+
+def threshold_image_manual(img:np.ndarray):
+    """
+    This function apply a threshold on the image given in params
     Parameters:
         img: an image
     Returns:
         a thresholded image as a ndarray
-    
+
     """
-    threshold_value=128
+    img = img_to_gray(img)
+    threshold_value = 128
+    img_out = np.zeros(img.shape, dtype=np.uint8)
     if img.dtype!=np.dtype(np.uint8):
         raise TypeError("le tableau doit être de type uint 8")
-    return img>threshold_value
+    for row in range(img.shape[0]):
+        for col in range(img.shape[1]):
+            for channel in range(img.shape[2]):
+                if (img[row, col, channel] > threshold_value  ):
+
+                    img_out[row, col, channel] = 255
+                else:
+                    img_out[row, col, channel] = 0
+    return img_out
+def threshold_image_numpy(img:np.ndarray):
+    """
+    This function apply a threshold on the image given in params
+    Parameters:
+        img: an image
+    Returns:
+        a thresholded image as a ndarray
+
+    """
+    if img.dtype!=np.dtype(np.uint8):
+        raise TypeError("le tableau doit être de type uint 8")
+    img = img_to_gray(img)
+    return  1.0 *  (img> 128)
+def threshold_colors_opencv(img):
+    """
+    This function apply a threshold on the image given in params
+    Parameters:
+        img: an image
+    Returns:
+        a thresholded image as a ndarray
+
+    """
+    ret, thresh2 = cv2.threshold(img, 127, 255, cv2.THRESH_OTSU)
+    return thresh2
 
 
 #img_gray=cv2.imread("testimg.jpg",0)
 
 #img_bgr=cv2.imread("testimg.jpg",1)
-img_bgr_reversed = threshold(cv2.imread("testimg.jpg",1))
-img_bgr_reversed = img_bgr_reversed.astype(np.uint8)*255
+#img_bgr_reversed = threshold(cv2.imread("testimg.jpg",1))
+#img_bgr_reversed = img_bgr_reversed.astype(np.uint8)*255
 #cv2.imshow("Gray levels image", img_gray)
 ##cv2.imshow("BGR image", img_bgr)
-cv2.imshow("BGR image thres", img_bgr_reversed+threshold(cv2.imread("testimg.jpg",1)))
+cv2.imshow("invert_colors_manual", invert_colors_manual(cv2.imread("testimg.jpg",1)))
+cv2.imshow("invert_colors_manualv2", invert_colors_manualv2(cv2.imread("testimg.jpg",1)))
+cv2.imshow("invert_colors_numpy", invert_colors_numpy(cv2.imread("testimg.jpg",1)))
+cv2.imshow("invert_colors_opencv", invert_colors_opencv(cv2.imread("testimg.jpg",1)))
+cv2.imshow("img_to_gray", img_to_gray(cv2.imread("testimg.jpg",1)))
+cv2.imshow("threshold_image_manual", threshold_image_manual(cv2.imread("testimg.jpg",1)))
+cv2.imshow("threshold_image_numpy", threshold_image_numpy(cv2.imread("testimg.jpg",1)))
+cv2.imshow("threshold_colors_opencv", threshold_colors_opencv(cv2.imread("testimg.jpg",0)))
 cv2.waitKey()
 
 #the tests are 
