b70798a91b7599c653ad02e1b2595fda03f6a626
bmx22c <github@bmx22c.fr>
2020-11-13 19:08:42 +0100

    added computer + style

--- assignements/S1_dice.py
+++ assignements/S1_dice.py
@@ -1,5 +1,6 @@
 import random
 import os
+import time
 
 dice = [1,2,3,4,5,6]
 isEnded = False
@@ -21,23 +22,21 @@ class colors:
     BOLD = '\033[1m'
     UNDERLINE = '\033[4m'
 
-def printc(str, multiline=False, color=""):
-    if color == "":
-        if tour % 2 == 1:
-            return print(colors.HEADER + str + colors.ENDC)
-        else:
-            return print(colors.OKGREEN + str + colors.ENDC)
-    else:
-        if multiline:
-            return print(color + str + colors.ENDC),
-        else:
-            return print(color + str + colors.ENDC)
+def printc(str):
+    return print(colors.HEADER + str + colors.ENDC)
 
 def inputc(str):
     if tour % 2 == 1:
         return input(colors.HEADER + str + colors.ENDC)
     else:
         return input(colors.OKGREEN + str + colors.ENDC)
+
+def getComputerAction():
+    choice = ""
+    if random.randint(1,4) == 1:
+        choice = "q"
+
+    return choice
 
 def shuffle(dice):
     if isinstance(dice, list):
@@ -47,6 +46,8 @@ os.system('cls' if os.name == 'nt' else 'clear')
 print(" \N{game die} DICE GAME \N{game die} ".center(consoleWidth - 2, "-"))
 
 while isEnded == False:
+    print("".center(consoleWidth, "-"))
+    print(colors.FAIL + "Score: Joueur ("+str(scoreJ)+") / Ordinateur ("+str(scoreO)+")" + colors.ENDC)
     if tour % 2 == 1:
         # Joueur
         if not dejaJoue:
@@ -62,9 +63,7 @@ while isEnded == False:
                 scoreTemp = 0
                 isSkip = True
             else:
-                printc("Tu as fait", True),
-                printc(str(pick), True, colors.UNDERLINE),
-                printc("! Veux-tu rejouer ?")
+                printc("Tu as fait " + str(pick) + " ! Veux-tu rejouer ?")
                 scoreTemp = scoreTemp + pick
                 printc("Score au tour actuel: " + str(scoreTemp))
                 dejaJoue = True
@@ -83,9 +82,42 @@ while isEnded == False:
     
     else:
         # Ordinateur
-        # Disons que l'ordinateur à 1 chance sur 4 de rejouer (à part si il tombe sur 1, bien sûr)
-        printc("Ordi tour")
-        printc(str(scoreJ))
-        dejaJoue = False
-        tour+=1
+        # Disons que l'ordinateur à 1 chance sur 4 de passer son tour (à part si il tombe sur 1, bien sûr)
+        # printc("Ordi tour")
+        # printc(str(scoreJ))
+        time.sleep(random.uniform(0.25,1.5))
+        # tour+=1
+        
+        isSkip = False
+        # print("L'ordinateur choisi une action: Jouer (Entrer) / Passer (q): ")
+        choice = getComputerAction()
+        # print("choice = " + choice)
+        if choice == "":
+            # Jouer
+            print("L'ordinateur a choisis de jouer.")
+            pick = shuffle(dice)
+            # print("pick = " + str(pick))
+            if pick == 1:
+                print("L'ordinateur as fait 1 ! Il ne peux donc pas rejouer et son score ne s'incrémentera pas. C'est à ton tour.")
+                scoreTemp = 0
+                isSkip = True
+            else:
+                print("Il as fait " + str(pick) + " ! Veux-t-il rejouer ?")
+                scoreTemp = scoreTemp + pick
+                print("Score au tour actuel: " + str(scoreTemp))
+        else:
+            # Passer son tour
+            print("L'ordinateur a choisi de passer son tour.")
+            tour+=1
+            scoreO+=scoreTemp
+
+        if isSkip == True:
+            tour+=1
+            scoreO+= scoreTemp
+
+        if scoreO >= 100:
+            isEnded = True
+            print("L'ordinateur a gagné !")
+
+    # print(tour)
 
